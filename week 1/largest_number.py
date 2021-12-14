62664f50b5f06f953ab164348f0b21076b3f6aaf
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2021-12-14 17:58:47 +0300

    Update largest_number.py

--- week 1/largest_number.py
+++ week 1/largest_number.py
@@ -1,13 +1,12 @@
-def largest_number(li):
-    li = [str(i) for i in li]
-    for i in range(len(li)):
-        count =0 
-        for j in range(len(li)-1):
-            num = int(''.join(li))
-            li[j], li[j+1] = li[j+1], li[j]
-            num2 = int(''.join(li))
-            if num > num2:
+class Solution:
+    def largestNumber(self, li):
+        li = [str(i) for i in li]
+        for i in range(len(li)):
+            for j in range(len(li)-1):
+                num = int(''.join(li))
                 li[j], li[j+1] = li[j+1], li[j]
-                count+=1
-        if count==0:
-            return ''.join(li)
+                num2 = int(''.join(li))
+                if num > num2:
+                    li[j], li[j+1] = li[j+1], li[j]
+        li = int(''.join(li))
+        return str(li)
