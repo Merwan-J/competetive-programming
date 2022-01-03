d728eacb1850f21cc50adee76b895f7c741d1d3a
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-01-03 22:56:21 +0300

    week-4 progress

---
+++ week 4/Find_Kth_Bit_in_Nth_Binary_String.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        def recur(s,i):
+            crnt = s + '1' 
+            s = [j for j in s]
+            for l in range(len(s)):
+                if s[l] == "0":
+                    s[l] = "1"
+                else:
+                    s[l] = "0"
+                    
+            s = crnt + "".join(s[::-1])
+            if i == n:
+                return s
+            return recur(s,i+1)
+        
+        return recur("0",1)[k-1]
