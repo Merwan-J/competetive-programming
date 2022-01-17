11098af10543f7cb742d252d1902bca791998eac
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-01-17 08:55:38 +0300

    Time: 52 ms (65.15%), Space: 15.3 MB (43.85%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -2,6 +2,7 @@ class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if k == 1:
             return head
+        
         result = []
         revArray = []
         
@@ -10,19 +11,19 @@ class Solution:
             _Next = head.next
             if head.next==None and len(revArray) != k:
                 result[-1].next = revArray[0]
-                result += revArray
                 
             if len(revArray)==k:
+                
                 for i in range(len(revArray)-1,-1,-1):   
-                    
                     if i==0:
                         revArray[i].next = None
                         result.append(revArray[i])
                         continue  
                         
                     revArray[i].next = revArray[i-1]
+                    
                     if i==len(revArray)-1 and len(result):
-                        result[-1].next = revArray[i]
+                        result[-1].next = revArray[-1]
                     result.append(revArray[i])
                     
                 revArray = []
