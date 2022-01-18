c0f8c844b90262d6cb6ec98d1c4002abe1c57792
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-01-19 02:38:21 +0300

    Time: 1113 ms (23.59%), Space: 39.3 MB (78.66%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,10 +5,39 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        arr = []
+#         This one takes O(N) Time and O(N) Space
+
+#         arr = []
         
-        while head:
-            arr.append(head.val)
+#         while head:
+#             arr.append(head.val)
+#             head = head.next
+        
+#         return arr==arr[::-1] 
+
+# But this code takes O(N) time and O(1) space
+# it first checks for the mid point 
+# then reverse the second half of the list
+# compares it to the first half of unreversed list
+# if they are compliment it means the list is palindrome 
+        fast,slow = head,head 
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        frontNode = None
+        nextNode = slow
+        
+        while nextNode:
+            _Next = nextNode.next
+            nextNode.next = frontNode
+            frontNode = nextNode
+            nextNode = _Next
+        
+        while frontNode:
+            if frontNode.val != head.val:
+                return False
+            frontNode = frontNode.next
             head = head.next
-        
-        return arr==arr[::-1] 
+        return True
