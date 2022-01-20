d56d89389391179405abad10efedb45b05bb7a0f
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-01-20 17:32:20 +0300

    Time: 115 ms (29.10%), Space: 15.5 MB (86.33%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,46 +1,21 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if len(height) == 1 or len(height)==2:
+        if len(height)<3:
             return 0
+        total = 0
         
-        l, r, total = 0, 1, 0
-        stack = []  
-        monotone = []
-        while l < len(height)-2:
-            if height[l] == 0:
-                l+=1
-                r = l+1
-                stack = []
-                monotone = []
-                continue 
-                
-            if height[r] >= height[l]:
-                while stack:
-                    total += min(height[l],height[r]) - stack[-1]
-                    stack.pop()
-                l = r
-                r = r+1
-                monotone = []
-                stack = []
-                continue  
-            if r>= len(height)-1:
-                stack.append(height[r])
-                monotone.append(r)
-                l+=1
-                r = l+1
-                
-                rMax = max(stack)
-                index = stack.index(rMax)
-                if index != 0:
-                    for i in range(index):
-                        total += rMax-stack[i]
-                    l = monotone[index]
-                    r = l+1
-               
-                stack = []
-                monotone = []
-                continue
-            monotone.append(r)
-            stack.append(height[r])
-            r+=1
+        maxValue = max(height)
+        maxIndex = height.index(maxValue)
+        
+        maxL=height[0]
+        maxR = height[-1]
+        
+        for i in range(1,maxIndex):
+            maxL = max(maxL,height[i])
+            total += maxL-height[i]
+        
+        for i in range(len(height)-2,maxIndex,-1):
+            maxR = max(maxR,height[i])
+            total += maxR-height[i]
+        
         return total
