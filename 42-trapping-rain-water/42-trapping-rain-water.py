65b7ad793877eeb3abfc84a933f3981227e4247d
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-01-20 17:36:36 +0300

    Time: 125 ms (23.19%), Space: 15.8 MB (29.29%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -4,17 +4,20 @@ class Solution:
             return 0
         total = 0
         
-        maxValue = max(height)
-        maxIndex = height.index(maxValue)
+        maxIndex = (0,0)
+        
+        for i in range(len(height)):
+            if height[i] > maxIndex[1]:
+                maxIndex = (i,height[i])
         
         maxL=height[0]
         maxR = height[-1]
         
-        for i in range(1,maxIndex):
+        for i in range(1,maxIndex[0]):
             maxL = max(maxL,height[i])
             total += maxL-height[i]
         
-        for i in range(len(height)-2,maxIndex,-1):
+        for i in range(len(height)-2,maxIndex[0],-1):
             maxR = max(maxR,height[i])
             total += maxR-height[i]
         
