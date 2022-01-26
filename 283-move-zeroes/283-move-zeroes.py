0e7f0a5a417cd5753a0f92b140d8f9c8ca80a950
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-01-26 16:48:44 +0300

    Time: 224 ms (44.74%), Space: 15.4 MB (70.89%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -14,10 +14,9 @@ class Solution:
         l,r = 0, 1
         
         while l<r and r<len(nums):
-            if nums[l]==0:
-                if nums[r]!=0:
-                    nums[l],nums[r] = nums[r],nums[l]
-                    l+=1
-            else:
+            if nums[l]==0 and nums[r]!=0:
+                nums[l],nums[r] = nums[r],nums[l]
+                l+=1
+            elif nums[l]!=0:
                 l+=1
             r+=1
