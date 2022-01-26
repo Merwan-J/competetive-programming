a760325877be9b908d39bea079bdf4834c018f93
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-01-26 16:30:46 +0300

    Time: 250 ms (37.73%), Space: 15.4 MB (32.64%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,9 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
+        """for i in range(len(nums)):
             if nums[i] == 0:
                 for j in range(i+1,len(nums)):
                     if nums[j]!=0:
                         nums[i],nums[j] = nums[j],nums[i]
                         break
+        """
+        
+        l,r = 0, 1
+        
+        while l<r and r<len(nums):
+            if nums[l]==0:
+                if nums[r]!=0:
+                    nums[l],nums[r] = nums[r],nums[l]
+                    l+=1
+            else:
+                l+=1
+            r+=1
