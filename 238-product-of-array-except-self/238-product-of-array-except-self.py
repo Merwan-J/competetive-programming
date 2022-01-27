ab768f255558cc487673ac1a8256d6248dfa1395
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-01-27 19:47:07 +0300

    Time: 536 ms (5.03%), Space: 21.2 MB (46.77%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -5,20 +5,36 @@ class Solution:
 #   suffix[24,12,4,1]
 #      arr[24,12,8,6]
 
-        prefix = [1]*len(nums)
-        suffix = [1]*len(nums)
+#         prefix = [1]*len(nums)
+#         suffix = [1]*len(nums)
+#         arr = []
+        
+# #       prefix
+#         for i in range(1,len(nums)):
+#             prefix[i] = prefix[i-1]*nums[i-1] 
+        
+# #       suffix
+#         for i in range(len(nums)-2,-1,-1):
+#             suffix[i] = suffix[i+1]*nums[i+1]
+        
+        
+#         for i in range(len(nums)):
+#             arr.append((prefix[i]*suffix[i]))
+        
+#         return arr
+        prefix = 1
+        suffix = 1
         arr = []
         
 #       prefix
-        for i in range(1,len(nums)):
-            prefix[i] = prefix[i-1]*nums[i-1] 
+        for i in range(len(nums)):
+            arr.append(prefix)
+            prefix = prefix*nums[i]
         
 #       suffix
-        for i in range(len(nums)-2,-1,-1):
-            suffix[i] = suffix[i+1]*nums[i+1]
+        for i in range(len(nums)-1,-1,-1):
+            arr[i] = arr[i]*suffix
+            suffix = suffix*nums[i]
         
-        
-        for i in range(len(nums)):
-            arr.append((prefix[i]*suffix[i]))
         
         return arr
