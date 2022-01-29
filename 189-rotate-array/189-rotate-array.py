14c0821157d77258f2526dcd2080243962eaccfe
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-01-29 11:56:07 +0300

    Time: 369 ms (32.20%), Space: 25.4 MB (84.30%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -4,13 +4,28 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        arr = [0]*len(nums)
+#         arr = [0]*len(nums)
         
-        for i in range(len(nums)):
-            arr[(i+k)%len(nums)] = nums[i]
+#         for i in range(len(nums)):
+#             arr[(i+k)%len(nums)] = nums[i]
             
-        for i in range(len(nums)):
-            nums[i] = arr[i]
+#         for i in range(len(nums)):
+#             nums[i] = arr[i]
+        
+        
+        def reverse(arr,left,right):
+            while left<right:
+                arr[left],arr[right] = arr[right],arr[left]
+                left += 1
+                right -= 1
+        
+# incase k is greater than the length of nums, this makes it in the range of the length
+        k = k % len(nums)
+        
+        reverse(nums,0,len(nums)-1)
+        reverse(nums,0,k-1)
+        reverse(nums,k,len(nums)-1)
+
             
         
         
