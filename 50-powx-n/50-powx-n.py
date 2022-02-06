e524af66e821c75685721210e666291c0912564b
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-02-06 09:56:15 +0300

    Time: 55 ms (18.09%), Space: 13.9 MB (96.64%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,26 +1,25 @@
-# class Solution:
-#     def myPow(self, x: float, n: int) -> float:
-#         if n==0:
-#             return x/x
-#         def helper(x,n):
-#             if n==1:
-#                 return x
-#             if n%2==0:
-#                 return helper(x,n//2)**2
-#             else:
-#                 return x * helper(x,n//2)**2
-        
-#         return 1/helper(x,-n) if n<0 else helper(x,n)
-
-
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-        if n < 0:
-            n *= -1
-            x = 1/x
-        if n%2 == 0:
-            return self.myPow(x,n//2)**2
-        return x*self.myPow(x,n//2)**2
+        if n==0:
+            return x/x
+        def helper(x,n):
+            if n==1:
+                return x
+            if n%2==0:
+                return helper(x,n//2)**2
+            return x * helper(x,n//2)**2
+        
+        return helper(1/x,-n) if n<0 else helper(x,n)
+
+
+# class Solution:
+#     def myPow(self, x: float, n: int) -> float:
+#         if n == 0:
+#             return 1
+#         if n < 0:
+#             n *= -1
+#             x = 1/x
+#         if n%2 == 0:
+#             return self.myPow(x,n//2)**2
+#         return x*self.myPow(x,n//2)**2
         
