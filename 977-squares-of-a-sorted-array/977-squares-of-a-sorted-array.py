272287bd5785f24cdafe7256638d2f8fac6d17da
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-02-07 10:21:15 +0300

    Time: 415 ms (20.10%), Space: 16.2 MB (35.64%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,42 +1,54 @@
 class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        arr = []
-        negatives = []
-        positives = []
+    # def sortedSquares(self, nums: List[int]) -> List[int]:
+    #     arr = []
+#         negatives = []
+#         positives = []
         
-        for i in range(len(nums)-1,-1,-1):
-            if nums[i]<0:
-                negatives.append(abs(nums[i]))
+#         for i in range(len(nums)-1,-1,-1):
+#             if nums[i]<0:
+#                 negatives.append(abs(nums[i]))
         
-        for i in nums:
-            if i>=0:
-                positives.append(i)
+#         for i in nums:
+#             if i>=0:
+#                 positives.append(i)
         
-        p,n  = 0,0
+#         p,n  = 0,0
         
-        while True:
-            if p<len(positives) and n<len(negatives):
-                if positives[p]<=negatives[n]:
-                    arr.append(positives[p])
-                    p+=1
-                else:
-                    arr.append(negatives[n])
-                    n+=1
+#         while True:
+#             if p<len(positives) and n<len(negatives):
+#                 if positives[p]<=negatives[n]:
+#                     arr.append(positives[p])
+#                     p+=1
+#                 else:
+#                     arr.append(negatives[n])
+#                     n+=1
                     
-            else:
-                if p>=len(positives) and n>=len(negatives):
-                    break
-                elif p>=len(positives):
-                    while n<len(negatives):
-                        arr.append(negatives[n])
-                        n+=1
-                    break
-                elif n>=len(negatives):
-                    while p<len(positives):
-                        arr.append(positives[p])
-                        p+=1
-                    break
-        for i in range(len(nums)):
-            arr[i] = arr[i]**2
+#             else:
+#                 if p>=len(positives) and n>=len(negatives):
+#                     break
+#                 elif p>=len(positives):
+#                     while n<len(negatives):
+#                         arr.append(negatives[n])
+#                         n+=1
+#                     break
+#                 elif n>=len(negatives):
+#                     while p<len(positives):
+#                         arr.append(positives[p])
+#                         p+=1
+#                     break
+#         for i in range(len(nums)):
+#             arr[i] = arr[i]**2
         
-        return arr
+#         return arr
+    def sortedSquares(self, A):
+        answer = collections.deque()
+        l, r = 0, len(A) - 1
+        while l <= r:
+            left, right = abs(A[l]), abs(A[r])
+            if left > right:
+                answer.appendleft(left * left)
+                l += 1
+            else:
+                answer.appendleft(right * right)
+                r -= 1
+        return list(answer)
