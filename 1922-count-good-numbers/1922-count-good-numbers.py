4a4dcd59ad6b8519e808f919f18e7464ad2c8db9
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-02-08 11:09:00 +0300

    Time: 52 ms (25.60%), Space: 13.9 MB (89.08%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,12 +1,24 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         N = 1000000007
-        # five = pow(5,)
-        # def helper(n):
-        #     if n==
-        if n%2==0:
-            return (pow(5,n//2,N)*pow(4,n//2,N))%N
-        else:
-            return (pow(5,(n//2)+1,N)*pow(4,n//2,N))%N
+        
+        def handler(power):
+            if power==0:
+                return 1
+            res = (handler(power//2))%N
+            res = (res*res)%N
+            return (res*20)%N if power%2 else res
+        
+        res = handler(n//2)
+        
+        return (res*5)%N if n%2 else res
+            
+        
+        
+        
+        # if n%2==0:
+        #     return (pow(5,n//2,N)*pow(4,n//2,N))%N
+        # else:
+        #     return (pow(5,(n//2)+1,N)*pow(4,n//2,N))%N
         
     
