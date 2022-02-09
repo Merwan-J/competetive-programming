8e8e3246ca9f1c7703a709ddd4651ff49da3f36b
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-02-09 06:50:11 +0300

    Time: 556 ms (5.05%), Space: 18.4 MB (93.99%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -3,23 +3,24 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-#         def helper(arr):
-#             if len(arr)==1:
-#                 return arr[0]
-#             return arr[-1] + helper(arr[:len(arr)-1])
+        def helper(arr):
+            if len(arr)==2:
+                return arr[-1]+arr[0]
+            if len(arr)==1:
+                return arr[0]
+            mid = len(arr)//2
+            return helper(arr[mid::]) + helper(arr[:mid])
         
-#         res = helper(s)
+        res = helper(s)
         
-#         for i in range(len(s)):
-#             s[i] = res[i]
+        for i in range(len(s)):
+            s[i] = res[i]
         
-#         return s
     
-        l,r = 0, len(s)-1
+#         l,r = 0, len(s)-1
         
-        while l<r:
-            s[l],s[r] = s[r],s[l]
-            l+=1
-            r-=1
-        return s
+#         while l<r:
+#             s[l],s[r] = s[r],s[l]
+#             l+=1
+#             r-=1
         
