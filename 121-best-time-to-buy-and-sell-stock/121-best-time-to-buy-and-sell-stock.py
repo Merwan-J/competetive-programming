36295cd3c7d9867d74efc7b066458c7e6cd64138
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-02-13 07:59:20 +0300

    Time: 1468 ms (44.94%), Space: 24.9 MB (92.23%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -6,14 +6,14 @@ class Solution:
         #     for j in range(i+1,len(prices)):
         #         if prices[i]<prices[j]:
         #             maxDiff = max(maxDiff,prices[j]-prices[i])
-                    
+        
+        maxDiff = 0
         l,r  = 0,1
         while r<len(prices):
             if prices[l]>prices[r]:
                 l = r
-                r = l+1
-            else:
-                maxDiff = max(maxDiff,prices[r]-prices[l])
-                r+=1
+                r = l
+            maxDiff = max(maxDiff,prices[r]-prices[l])
+            r+=1
         
         return maxDiff
