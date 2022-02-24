e5a6a4fa551619dc2095bf4b0f052aed99be9380
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-02-24 12:48:49 +0300

    Time: 126 ms (62.78%), Space: 18.8 MB (32.96%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,17 +1,31 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        nums.sort()
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        
+#         hashMap = {}
+#         count = 1
         
-        hashMap = {}
-        count = 1
+#         for num in nums:
+#             hashMap[num] = hashMap.get(num,0) + 1
+
+#         hashMap = dict(sorted(hashMap.items(),key=lambda item: item[1],reverse=True))
+#         arr = []
         
-        for num in nums:
-            hashMap[num] = hashMap.get(num,0) + 1
-        hashMap = dict(sorted(hashMap.items(),key=lambda item: item[1],reverse=True))
-        print(hashMap)
-        arr = []
-        for key,value in hashMap.items():
-            arr.append(key)
-            if len(arr)==k:
-                return arr
+#         for key,value in hashMap.items():
+#             arr.append(key)
+#             if len(arr)==k:
+#                 return arr
+        counter = dict()
+    
+        for i in nums:
+            counter[i] = counter.get(i,0)+1
+        ans = []
+        for key,value in counter.items():
+            ans.append((value,key))
         
+        heapq.heapify(ans)
+        
+        while len(ans)>k:
+            heapq.heappop(ans)
+        
+        for i in range(len(ans)):
+            ans[i] = ans[i][1]
+        return ans
