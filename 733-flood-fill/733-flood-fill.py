81b430ab80b420d134325bbd8d0498857f2378b1
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-03-05 01:19:45 +0300

    Time: 80 ms (82.67%), Space: 14.4 MB (33.16%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,17 +1,15 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        visited = dict()
         def isvalid(r,c):
             if r<len(image) and r>-1 and c>-1 and c<len(image[0]):
                 return True
             return False
         
         def fill(r,c,color):
-            if not isvalid(r,c) or (r,c) in visited:
+            if not isvalid(r,c) or image[r][c]==newColor:
                 return 
             if image[r][c] == color: 
                 image[r][c]  = newColor
-                visited[(r,c)] = True
 
                 fill(r,c-1,color)
                 fill(r,c+1,color)
