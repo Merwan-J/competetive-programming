79f3f9dc57b0c8dc59b3eabf0e7054ce3b7d2cf6
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-03-23 10:01:33 +0300

    Time: 40 ms (91.59%), Space: 13.9 MB (28.02%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,8 +1,5 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        
-#         move the ones that have zero count to crntTrue and compare the length of it to the crnt set at each sliding window.... 
-# the other naive way of doing it just by having crnt set being checked weather each elt's count is zero or not
 
 
         counter = dict()
@@ -10,7 +7,6 @@ class Solution:
         
         for i in range(len(s)):
             counter[s[i]] = i
-        
         
         l,r = 0,0
         
