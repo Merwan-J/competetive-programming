bc1c4ba1be1bb4caa75ed871d5c9fa6acd0f9b23
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-05-15 20:19:57 +0300

    Time: 153 ms (51.16%), Space: 18 MB (66.28%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.py
+++ 275-h-index-ii/275-h-index-ii.py
@@ -4,28 +4,33 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-#         [0,1,3,5,6]
+#         [ 0, 1, 3, 5, 6]
+            # 0, 1 ,2  3  4
+                
 #         [ 6, 5 , 3 , 1 , 0]
 #           0  1   2   3   4
 #         [ 10, 8, 5,  4,   3, 3, 2, 1, 1]
         
 #           0   1  2    3   4  5  6  7  8
-        if (len(citations)==1 and citations[0]>=1) or (len(set(citations))==1 and 1 in set(citations)):
-            return 1
+        
+        daset = set(citations)
+        if len(daset)==1:
+            if (len(citations)==1 and citations[0]>=1) or (len(daset)==1 and citations[0]==1):
+                return 1
+            
         
         l,r = 0,len(citations)
-        citations = citations[::-1]
         
         h = 0
         
         while l<=r and l<len(citations) and r>=0:
             mid = (l+r)//2  
                                                                                             
-            if citations[mid]>=mid+1:
-                h = mid+1
-                l = mid + 1
-            else:
+            if citations[mid]>=len(citations)-mid:
+                h = len(citations)-mid
                 r = mid - 1
+            else:
+                l = mid + 1
         
         return h
         
