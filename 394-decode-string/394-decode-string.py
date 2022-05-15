98696685817a813f9345c15514fc1643a7f45944
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-05-15 08:05:43 +0300

    Time: 44 ms (41.50%), Space: 13.9 MB (71.59%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -37,32 +37,27 @@ class Solution:
     
         def helper(s,i,ans):
             if i>= len(s):
-                print("shit is over")
                 return ans,i
             
             if s[i]==']':
-                print("brace closed")
                 return ans,i
             
             elif s[i]=='[':
-                print("it is opening brace")
                 return helper(s,i+1,ans)
+            
             elif s[i].isdigit():
-                print(s[i])
                 n = ''
-                
                 for j in range(i,len(s)):
                     if not s[j].isdigit():
                         break
                     n += s[j]
-                print(n,"its a digit")
+                    
                 temp,i = helper(s,j,"")
-                print(temp,i,"multilpy this shit with",n)
                 ans += int(n)*temp
+                
                 return helper(s,i+1,ans)
             
             else:
-                print(s[i],"just a letter, append it")
                 return helper(s,i+1,ans+s[i])
         
         return helper(s,0,"")[0]
