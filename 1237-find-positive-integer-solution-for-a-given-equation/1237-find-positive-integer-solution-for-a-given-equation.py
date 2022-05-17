6b9a6aa2dfa9f2c9adc2b5bcb2a2dee631f81a54
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-05-17 09:33:12 +0300

    Time: 312 ms (7.51%), Space: 14 MB (46.45%) - LeetHub

--- 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.py
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.py
@@ -13,22 +13,23 @@ class Solution:
     def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
         ans = []
         for x in range(1,1001):
-#             l,r = x,1001
+            l,r = 1,1001
             
-#             while l<=r:
-#                 mid = (l+r)//2
-#                 result = customfunction.f(x,mid)
-#                 if result==z:
-#                     ans.append([x,mid])
-#                 elif result < z:
-#                     l = mid + 1
-#                 else:
-#                     r = mid - 1
-            for y in range(1,1001):
-                result = customfunction.f(x,y)
+            while l<=r:
+                mid = (l+r)//2
+                result = customfunction.f(x,mid)
                 if result==z:
-                    ans.append([x,y])
+                    ans.append([x,mid])
                     break
-                elif result>z:
-                    break
+                elif result < z:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            # for y in range(1,1001):
+            #     result = customfunction.f(x,y)
+            #     if result==z:
+            #         ans.append([x,y])
+            #         break
+            #     elif result>z:
+            #         break
         return ans
