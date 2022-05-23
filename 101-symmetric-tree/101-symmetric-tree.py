9a0b586acd6c116403016d9b92b4037b21341290
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-05-23 18:37:41 +0300

    Time: 35 ms (30.57%), Space: 13.5 MB (69.34%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -10,28 +10,37 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        LeftNodes = []
-        RightNodes = []
+#         LeftNodes = []
+#         RightNodes = []
         
-        def right(node):
-            if node is None:
-                RightNodes.append(None)
-                return 
-            RightNodes.append(node.val)
+#         def right(node):
+#             if node is None:
+#                 RightNodes.append(None)
+#                 return 
+#             RightNodes.append(node.val)
             
-            right(node.right)
-            right(node.left)
+#             right(node.right)
+#             right(node.left)
         
-        def left(node):
-            if node is None:
-                LeftNodes.append(None)
-                return
-            LeftNodes.append(node.val)
+#         def left(node):
+#             if node is None:
+#                 LeftNodes.append(None)
+#                 return
+#             LeftNodes.append(node.val)
             
-            left(node.left)
-            left(node.right)
+#             left(node.left)
+#             left(node.right)
         
-        right(root)
-        left(root)
-        return LeftNodes==RightNodes
+#         right(root)
+#         left(root)
+#         return LeftNodes==RightNodes
         
+        def isMirror(node1,node2):
+            if node1 is None and node2 is None:
+                return True
+            if node1 is None or node2 is None:
+                return False
+            
+            return node1.val==node2.val and isMirror(node1.left,node2.right) and isMirror(node1.right,node2.left)
+        
+        return isMirror(root,root)
