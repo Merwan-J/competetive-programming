397d2cbe49852d17cfde02cbc9f51de5dd2d8ff0
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-05-23 17:04:00 +0300

    Time: 59 ms (5.70%), Space: 13.9 MB (5.92%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -1,56 +1,37 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        print(root.left,root.right)
-        arr1 = []
-        arr2 = []
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        LeftNodes = []
+        RightNodes = []
         
         def right(node):
             if node is None:
-                arr2.append(None)
-                return None
+                RightNodes.append(None)
+                return 
+            RightNodes.append(node.val)
             
-            if node.right is None and node.left is None:
-                arr2.append(node.val)
-                return None
-            arr2.append(node.val)
-            
-            if node.right:
-                right(node.right)
-            else:
-                arr2.append(None)
-            
-            if node.left:
-                right(node.left)
-            else:
-                arr2.append(None)
-            
-           
+            right(node.right)
+            right(node.left)
         
         def left(node):
             if node is None:
-                arr1.append(None)
-                return None
-            if node.right is None and node.left is None:
-                arr1.append(node.val)
-                return None
-            arr1.append(node.val)
-            if node.left:
-                left(node.left)
-            else:
-                arr1.append(None)
+                LeftNodes.append(None)
+                return
+            LeftNodes.append(node.val)
             
-            if node.right:
-                left(node.right)
-            else:
-                arr1.append(None)
+            left(node.left)
+            left(node.right)
         
-        right(root.right)
-        left(root.left)
-        return arr1==arr2
+        right(root)
+        left(root)
+        return LeftNodes==RightNodes
         
