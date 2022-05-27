13345e8ad74e6c9a64e1e53eec376c266aadc5e9
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-05-27 21:56:44 +0300

    Time: 1994 ms (39.62%), Space: 52.1 MB (88.38%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -1,16 +1,47 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda item: item[1])
-        i = 0
+#         intervals.sort(key=lambda item: item[1])
+#         i = 0
+#         count = 0
+#         print(intervals)
+#         while i<len(intervals):
+#             end = intervals[i][1]
+#             i += 1
+#             while i<len(intervals) and end>intervals[i][0]:
+#                 count += 1
+#                 i+=1
+        
+#         return count
+    
+    
+        intervals.sort(key=lambda item: item[0])
         count = 0
-        print(intervals)
-        while i<len(intervals):
-            end = intervals[i][1]
-            i += 1
-            while i<len(intervals) and end>intervals[i][0]:
+        prevEnd = intervals[0][1]
+        for start,end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
                 count += 1
-                
-                # print(end,intervals[i][0])
-                i+=1
+                prevEnd = min(prevEnd,end)
         
         return count
+    
+    
+    
+#     idea: to find the minimum deletion to create a non overlapping intervals
+
+# intution: 
+
+# sort the intervals by thier start value, and check wether the adjecent intervals in the sorted array are overlapping or not. 
+# the check for overlapping can be done by comparing the end point of the first interval with the start point of the second interval.
+
+# if the two intervals are overlapping, one of them has to be removed. The one that is going to be removed is the interval with smalles ending point.
+# 	i.e there will be less chance that the one remaing interval will overlap with the next comming one if it ends first. 
+# 		since the whole idea is to minimize deletion
+
+# we have to also save the smallest ending point so that we can compare it to the next interval.
+
+# we could do this question with the same concepth but different way by sorting the intervals by their ending point
+
+
+
