d90ceae32eabccc459d26c75a6692b94d41ec9bd
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-06-02 10:44:07 +0300

    Time: 70 ms (32.90%), Space: 14 MB (73.96%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,18 +1,35 @@
 class Solution:
     def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
-        dp = {}
+#         Top down approach
+
+#         dp = {}
         
-        def dfs(row,col):
-            if row>=len(grid) or col>=len(grid[0]) or grid[row][col]==1:
-                return 0
+#         def dfs(row,col):
+#             if row>=len(grid) or col>=len(grid[0]) or grid[row][col]==1:
+#                 return 0
             
-            if row==len(grid)-1 and col == len(grid[0])-1:
-                return 1
-            if (row,col) in dp:
-                return dp[(row,col)]
+#             if row==len(grid)-1 and col == len(grid[0])-1:
+#                 return 1
+#             if (row,col) in dp:
+#                 return dp[(row,col)]
             
-            dp[(row,col)] = dfs(row+1,col) + dfs(row,col+1)
+#             dp[(row,col)] = dfs(row+1,col) + dfs(row,col+1)
             
-            return dp[(row,col)]
+#             return dp[(row,col)]
         
-        return dfs(0,0)
+#         return dfs(0,0)
+
+
+#  Bottom up approach
+        row = len(grid)
+        col = len(grid[0])
+        result = [[0]*(col+1) for i in range(row+1)]
+        result[row][col-1] = 1
+        
+        for r in range(row-1,-1,-1):
+            for c in range(col-1,-1,-1):
+                if grid[r][c] == 0:
+                    result[r][c] = result[r+1][c] + result[r][c+1]
+                
+        return result[0][0]
+        
