de95e1fae1f396bbef32c63c131452e0b7300081
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-06-06 09:44:35 +0300

    Time: 62 ms (15.61%), Space: 14.1 MB (18.85%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,23 +1,47 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # Array to store the subproblem results
-        dp = [0 for _ in range(len(s) + 1)]
+#         # Array to store the subproblem results
+#         dp = [0 for _ in range(len(s) + 1)]
 
-        dp[0] = 1
-        # Ways to decode a string of size 1 is 1. Unless the string is '0'.
-        # '0' doesn't have a single digit decode.
-        dp[1] = 0 if s[0] == '0' else 1
+#         dp[0] = 1
+#         # Ways to decode a string of size 1 is 1. Unless the string is '0'.
+#         # '0' doesn't have a single digit decode.
+#         dp[1] = 0 if s[0] == '0' else 1
 
 
-        for i in range(2, len(dp)):
+#         for i in range(2, len(dp)):
 
-            # Check if successful single digit decode is possible.
-            if s[i - 1] != '0':
-                dp[i] = dp[i - 1]
+#             # Check if successful single digit decode is possible.
+#             if s[i - 1] != '0':
+#                 dp[i] = dp[i - 1]
 
-            # Check if successful two digit decode is possible.
-            two_digit = int(s[i - 2 : i])
-            if two_digit >= 10 and two_digit <= 26:
-                dp[i] += dp[i - 2]
+#             # Check if successful two digit decode is possible.
+#             two_digit = int(s[i - 2 : i])
+#             if two_digit >= 10 and two_digit <= 26:
+#                 dp[i] += dp[i - 2]
                 
-        return dp[len(s)]
+#         return dp[len(s)]
+        
+        memo = {}
+        
+        def check(index):
+            if index == len(s):
+                return 1
+            
+            if s[index]== '0':
+                return 0
+            
+            if index in memo:
+                return memo[index]
+            
+            ans = check(index+1)
+    
+            num = int(s[index:index+2])
+            if num<=26 and num>=10:
+                ans += check(index+2)
+            
+            memo[index] = ans
+            
+            return memo[index]
+        
+        return check(0)
