6f5c3f4d36434936f55881154d03c78b4d2f478d
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-06-16 15:16:27 +0300

    Time: 7700 ms (5.00%), Space: 119.1 MB (26.55%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points) #number of nodes
+        visited = [False]*n #to keep track of visited nodes
+        heap = [] # to maintain the lowest cost edges
+        graph = collections.defaultdict(list) # to store each edges of a given vertex
+        getCost = lambda p1,p2: abs(p1[0]-p2[0]) + abs(p1[1]-p2[1]) #calculate the cost of an edge
+        mstcost = 0 #to keep track of the minimum cost
+        
+        for i in range(n):
+            for j in range(i+1,n):
+                cost = getCost(points[i],points[j])
+                graph[i].append((cost,j))
+                graph[j].append((cost,i))
+        
+        heap = graph[0]
+        heapq.heapify(graph[0])
+        visited[0]  = True
+        
+        print(heap)
+        
+        while heap and n:
+            cost,node = heapq.heappop(heap)
+            if visited[node]:
+                continue
+            visited[node] = True
+            mstcost += cost
+            n-=1
+        
+            for edge in graph[node]:
+                if not visited[edge[1]]:
+                    heapq.heappush(heap,edge)
+        
+        return mstcost
+            
