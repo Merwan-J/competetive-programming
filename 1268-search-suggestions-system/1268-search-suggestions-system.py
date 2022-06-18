fa363f59f9ca64f597d497c5775a5721b4fa8ab9
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-06-18 19:26:06 +0300

    Time: 242 ms (56.30%), Space: 21.5 MB (22.44%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,43 +1,68 @@
-class Trie:
-    def __init__(self):
-        self.nodes = defaultdict(Trie)
-        self.word = None
+# class Trie:
+#     def __init__(self):
+#         self.nodes = defaultdict(Trie)
+#         self.word = None
     
-    def insert(self,word):
-        cur = self
-        for char in word:
-            cur = cur.nodes[char]
-        cur.word = word
+#     def insert(self,word):
+#         cur = self
+#         for char in word:
+#             cur = cur.nodes[char]
+#         cur.word = word
     
-    def getWords(self):
-        words = []
+#     def getWords(self):
+#         words = []
         
-        def dfs(root):
-            if len(words)==3:
-                return
-            if root.word:
-                words.append(root.word)
+#         def dfs(root):
+#             if len(words)==3:
+#                 return
+#             if root.word:
+#                 words.append(root.word)
             
-            for char in ascii_lowercase:
-                if char in root.nodes:
-                    dfs(root.nodes[char])
+#             for char in ascii_lowercase:
+#                 if char in root.nodes:
+#                     dfs(root.nodes[char])
             
-        dfs(self)
-        return words
+#         dfs(self)
+#         return words
+
+# class Solution:
+#     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+#         trie = Trie()
+#         for word in products:
+#             trie.insert(word)
+        
+#         ans = []
+#         cur = trie
+#         for char in searchWord:
+#             if cur is not None and char in cur.nodes:
+#                 cur = cur.nodes[char]
+#                 ans.append(cur.getWords())
+#             else:
+#                 cur = None
+#                 ans.append([])
+#         return ans          
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        trie = Trie()
-        for word in products:
-            trie.insert(word)
+        class TrieNode:
+            def __init__(self):
+                self.children = collections.defaultdict(TrieNode)
+                self.suggestion = []
+            
+            def add_sugestion(self, product):
+                if len(self.suggestion) < 3:
+                    self.suggestion.append(product)
         
-        ans = []
-        cur = trie
+        products = sorted(products)
+        root = TrieNode()
+        for p in products:
+            node = root
+            for char in p:
+                node = node.children[char]
+                node.add_sugestion(p)
+        
+        result, node = [], root
         for char in searchWord:
-            if cur is not None and char in cur.nodes:
-                cur = cur.nodes[char]
-                ans.append(cur.getWords())
-            else:
-                cur = None
-                ans.append([])
-        return ans                
+            node = node.children[char]
+            result.append(node.suggestion)
+        return result
