9f4c886a9cd6904433a3751ae0b47a0abedb60bd
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-06-30 09:01:38 +0300

    Time: 69 ms (95.40%), Space: 13.8 MB (97.65%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
@@ -8,25 +8,33 @@ class Solution:
         
 #         return count
 
-        counter = collections.defaultdict(list)
-        seen = set()
+#         counter = collections.defaultdict(list)
+#         seen = set()
+#         count = 0
+        
+#         for i in range(len(nums)):
+#             num = nums[i]
+#             counter[num].append(i)
+#         for i in range(len(nums)):
+#             num = nums[i]
+            
+#             j = -1
+#             while num-k in counter and j>=-len(counter[num-k]) and counter[num-k][j]>i:
+#                 count+=1
+#                 j-=1
+            
+#             j = -1
+#             while num+k in counter and j>=-len(counter[num+k]) and counter[num+k][j]>i:
+#                 count+=1
+#                 j-=1
+            
+        
+#         return count
+
+        seen = collections.defaultdict(int)
         count = 0
         
-        for i in range(len(nums)):
-            num = nums[i]
-            counter[num].append(i)
-        for i in range(len(nums)):
-            num = nums[i]
-            
-            j = -1
-            while num-k in counter and j>=-len(counter[num-k]) and counter[num-k][j]>i:
-                count+=1
-                j-=1
-            
-            j = -1
-            while num+k in counter and j>=-len(counter[num+k]) and counter[num+k][j]>i:
-                count+=1
-                j-=1
-            
-        
+        for num in nums:
+            count+=seen[num-k] + seen[num+k]
+            seen[num]+=1
         return count
