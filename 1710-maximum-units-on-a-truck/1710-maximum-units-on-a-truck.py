06010a6e851114255e40900844e9d1b50f8cc6b9
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-07-01 09:55:17 +0300

    Time: 3357 ms (5.01%), Space: 14.5 MB (32.64%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,28 +1,14 @@
 class Solution:
-    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-#         count = 0
+    def maximumUnits(self, boxs: List[List[int]], truckSize: int) -> int:
+        boxs.sort(key=lambda box: box[1])        
+        ans = 0
         
-#         boxTypes.sort(key=lambda item: item[1])
-        
-#         while boxTypes and truckSize > 0:
-#             boxTypes[-1][0] -= 1
-#             count += boxTypes[-1][1]
-#             truckSize -= 1
+        while boxs and truckSize:
+            ans += boxs[-1][1]
+            boxs[-1][0]-=1
+            truckSize-=1
             
-#             if boxTypes[-1][0]==0:
-#                 boxTypes.pop()
-            
+            if boxs[-1][0] == 0:
+                boxs.pop()
         
-#         return count
-        boxes, cur_units, cnt = 0, 1000, Counter()
-        for box, units in boxTypes:
-            cnt[units] += box
-        while cur_units > 0:
-            if cnt[cur_units] > 0:
-                fit_in = min(truckSize, cnt[cur_units])    
-                boxes += fit_in * cur_units
-                truckSize -= fit_in
-                if truckSize == 0:
-                    return boxes
-            cur_units -= 1
-        return boxes
+        return ans
