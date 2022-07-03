ae1f984109dd71f93bcea5cd8f386694cb1aae9f
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-07-03 12:27:40 +0300

    Time: 154 ms, Memory: 15.4 MB - LeetHub

---
+++ search-for-a-range/search-for-a-range.py
@@ -0,0 +1,31 @@
+class Solution:
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums:
+            return [-1, -1]
+
+        def bisect_left(nums, target):
+            l, r = 0, len(nums) - 1
+            while l < r:
+                m = (l + r) // 2
+                if nums[m] < target:
+                    l = m + 1
+                else:
+                    r = m
+            return l if nums[l] == target else -1
+
+        def bisect_right(nums, target):
+            l, r = 0, len(nums) - 1
+            while l < r:
+                m = (l + r) // 2 + 1
+                if nums[m] > target:
+                    r = m - 1
+                else:
+                    l = m
+            return l if nums[l] == target else -1
+
+        return [bisect_left(nums, target), bisect_right(nums, target)]
