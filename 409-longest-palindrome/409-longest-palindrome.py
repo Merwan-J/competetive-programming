f2527e3cafe5a9f1090aad1d566a028ab4e94ebe
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-07-06 11:53:05 +0300

    Time: 29 ms (98.05%), Space: 13.9 MB (21.91%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -2,16 +2,14 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         counter = collections.Counter(s)
         n = len(counter)
-        
         size = 0
         
         for key,val in counter.items():
             size += (val//2)*2
             counter[key] = val%2
-            
             if counter[key] == 0:
                 n-=1
-        
+                
         if size%2 == 0 and n>0:
             size += 1
         
