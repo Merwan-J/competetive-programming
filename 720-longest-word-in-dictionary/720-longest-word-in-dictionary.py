207e565e170f1921fe278cab5ac07723d8cf52b5
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-08-16 15:06:19 +0300

    Time: 204 ms (47.55%), Space: 14.7 MB (56.68%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
@@ -1,58 +1,45 @@
 class TrieNode:
     def __init__(self):
-        self.nodes = dict()
-        self.word = None
+        self.children = {}
+        self.end = False
+        
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
-    def insert(self,words):
-        ans = set()
+        self.node = TrieNode()
+        self.words = []
         
-        for word in words:
-            root = self.root
-            lastWord = None
-            add = True
-            
-            for i in range(len(word)):
-                char = word[i]
-                if char not in root.nodes and i==len(word)-1:
-                    root.nodes[char] = TrieNode()
-                elif char not in root.nodes and i!= len(word)-1:
-                    add = False
+    def insert(self,word):
+        flag = False
+        cur = self.node
+        
+        for i in range(len(word)):
+            char = word[i]
+            if char not in cur.children:
+                if i!=len(word)-1:
+                    flag = True
                     break
                     
-                root = root.nodes[char]
-                
-                if root.word:
-                    lastWord = root.word
-            # print(word,add)
-            if add:
-                root.word = word
-                ans.add(word)
-                
-                if lastWord and lastWord in ans: 
-                    ans.remove(lastWord)
-                
-            
-            
+                else:
+                    cur.children[char] = TrieNode()
+                    cur.end = True
+            cur = cur.children[char]
         
-        ans = sorted(list(ans),key=lambda i: len(i),reverse=True)
-        return ans
+        if not flag:
+            self.words.append(word)
             
-            
+
 class Solution:
     def longestWord(self, words: List[str]) -> str:
         words.sort()
-        # print(words)
         
-        ans = Trie().insert(words)
-        longest = ans[0] if ans else ""
-        # print(ans)
+        t = Trie()
         
-        for word in ans:
-            if len(word) == len(longest) and word<longest:
-                longest = word
-                
-        return longest
+        for word in words:
+            t.insert(word)
+        
+        words = sorted(t.words,key=lambda item: (-len(item),item))
+        
+        return "" if words==[] else words[0]
+        
         
         
