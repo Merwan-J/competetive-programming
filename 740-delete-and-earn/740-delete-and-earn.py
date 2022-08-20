9de0d8b4810a2ee751c75590b06a23dd44e2c24e
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-08-20 23:49:26 +0300

    Time: 137 ms (55.75%), Space: 15.9 MB (77.46%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,16 +1,27 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         counter = collections.Counter(nums)
-        @cache
-        def maxPoints(num):
-            if num == 0:
-                return 0
-            if num == 1:
-                return counter[1]
-            
-            return max(counter[num]*num+maxPoints(num-2),maxPoints(num-1))
+        dp = [0]*(max(nums)+1)
+        dp[1] = counter[1]
         
-        return maxPoints(max(nums))
+        for num in range(2,len(dp)):
+            dp[num] = max(dp[num-1],dp[num-2]+counter[num]*num)
+        
+        return dp[-1]
+        
+        
+        
+#         counter = collections.Counter(nums)
+#         @cache
+#         def maxPoints(num):
+#             if num == 0:
+#                 return 0
+#             if num == 1:
+#                 return counter[1]
+            
+#             return max(counter[num]*num+maxPoints(num-2),maxPoints(num-1))
+        
+#         return maxPoints(max(nums))
         
         
 #         counter = collections.Counter(nums)
