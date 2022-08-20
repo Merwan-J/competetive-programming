de30faa004015bc8c1aea16a2abf5de1ef381f37
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-08-20 23:38:36 +0300

    Time: 90 ms (94.01%), Space: 23.1 MB (19.12%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,17 +1,30 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         counter = collections.Counter(nums)
-        nums = sorted(list(set(nums)))
-        memo = {}
-
         @cache
-        def helper(i,deleted,):
-            if i >= len(nums):
+        def maxPoints(num):
+            if num == 0:
                 return 0
-         
-            dontPick = helper(i+1,False)
-            pick = counter[nums[i]]*nums[i] + helper(i+1,True) if not(deleted and nums[i-1]+1==nums[i]) else 0
+            if num == 1:
+                return counter[1]
             
-            return max(dontPick,pick)
+            return max(counter[num]*num+maxPoints(num-2),maxPoints(num-1))
         
-        return helper(0,False)
+        return maxPoints(max(nums))
+        
+        
+#         counter = collections.Counter(nums)
+#         nums = sorted(list(set(nums)))
+#         memo = {}
+
+#         @cache
+#         def helper(i,deleted,):
+#             if i >= len(nums):
+#                 return 0
+         
+#             dontPick = helper(i+1,False)
+#             pick = counter[nums[i]]*nums[i] + helper(i+1,True) if not(deleted and nums[i-1]+1==nums[i]) else 0
+            
+#             return max(dontPick,pick)
+        
+#         return helper(0,False)
