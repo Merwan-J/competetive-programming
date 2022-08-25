b504e1069a111caff63dea3e9bc7a863d4c5aa17
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-08-25 08:41:48 +0300

    Time: 62 ms (63.81%), Space: 14.8 MB (47.43%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,25 +1,22 @@
 class Solution:
     def isInterleave(self, s: str, t: str, p: str) -> bool:
-        if len(s)+len(t)<len(p):
-            return False
-        if len(s)+len(t)>len(p):
-            return False
+        if len(s)+len(t) != len(p):
+            return False 
         
         @cache
-        def dp(i_p,i_s,i_t):
-            if i_p>=len(p):
+        def dp(m,n):
+            if m+n>=len(p):
                 return True
             
-            if i_s>=len(s) and i_t>=len(t):
+            if m>=len(s) and n>=len(t):
                 return True
             
             ans = False
-            
-            if i_s<len(s) and p[i_p] == s[i_s]:
-                ans = dp(i_p+1,i_s+1,i_t)
-            if i_t<len(t) and p[i_p] == t[i_t]:
-                ans = ans or dp(i_p+1,i_s,i_t+1)
+            if m<len(s) and p[m+n] == s[m]:
+                ans = dp(m+1,n)
+            if n<len(t) and p[m+n] == t[n]:
+                ans = ans or dp(m,n+1)
             
             return ans
         
-        return dp(0,0,0)
+        return dp(0,0)
