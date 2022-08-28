d073fe9888510e3faf3116e7bb7eea4e1dd7c667
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-08-28 20:52:20 +0300

    Time: 1631 ms (5.81%), Space: 17.1 MB (24.20%) - LeetHub

---
+++ 221-maximal-square/221-maximal-square.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        h,w = len(matrix),len(matrix[0])
+        
+        dp = [[0]*w for _ in range(h)]
+        ans = 0
+        
+        for row in range(h):
+            for col in range(w):
+                dp[row][col] = int(matrix[row][col])
+                if dp[row][col] == 1 and row>0 and col>0:
+                    dp[row][col] += min(dp[row-1][col],dp[row][col-1],dp[row-1][col-1])
+                ans = max(ans,dp[row][col])
+        
+        return ans * ans
+            
+        
+        
