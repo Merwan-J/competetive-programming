c6e956caeb29a70dcbc30b8b5786c7dec7713e49
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-08-31 14:30:03 +0300

    Time: 426 ms (96.34%), Space: 19.9 MB (11.27%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,44 +1,22 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        m = len(matrix)
-        n = len(matrix[0])
-        
-        graph = defaultdict(list)
+        m,n = len(matrix),len(matrix[0])
         valid = lambda row,col: row>-1 and row<m and col>-1 and col<n
         
-        indegree = [[0]*n for _ in range(m)]
-
-        for row in range(m):
-            for col in range(n):
-                num = matrix[row][col]
-                up,down,right,left = [],[],[],[]
-                for a,b in [(-1,0),(1,0),(0,1),(0,-1)]:
-                    r,c = row+a,col+b
-                    if valid(r,c) and num < matrix[r][c]:
-                        graph[(row,col)] += [(r,c)]
-                        indegree[r][c] += 1
-        q = deque([])
-        for row in range(m):
-            for col in range(n):
-                if indegree[row][col] == 0:
-                    q.append((row,col))
-        
         @cache
-        def dfs(r,c):
-            if graph[(r,c)] == []:
-                return 1
-            ans = 0
-            for row,col in graph[(r,c)]:
-                ans = max(ans,dfs(row,col))
+        def dfs(row,col):
+            num = matrix[row][col]
             
-            return 1 + ans
+            up = dfs(row-1,col) if valid(row-1,col) and num<matrix[row-1][col] else 0    
+            down = dfs(row+1,col) if valid(row+1,col) and num<matrix[row+1][col] else 0   
+            left =dfs(row,col-1) if valid(row,col-1) and num<matrix[row][col-1] else 0   
+            right = dfs(row,col+1) if valid(row,col+1) and num<matrix[row][col+1] else 0    
+            return 1 + max(up,left,right,down)
         
         ans = 0
-        while q:
-            r,c = q.popleft()
-            ans = max(ans,dfs(r,c))
+        
+        for row in range(m):
+            for col in range(n):
+                ans = max(ans,dfs(row,col))
         
         return ans
-            
-
-               
