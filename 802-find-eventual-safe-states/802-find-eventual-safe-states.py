604c9be69e423d39fabb732c07bea01e990124c0
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-01 08:26:29 +0300

    Time: 1714 ms (5.04%), Space: 21.7 MB (67.83%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.py
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.py
@@ -1,56 +1,37 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-#         state: 0 means good to process 1: means is being processes currently, helps detect cycle
-
-#         state = [0]*len(graph)  
-#         memo = {}
-        
-#         def dfs(i):
-#             if state[i]==1:
-#                 return False
-            
-#             if i in memo:
-#                 return memo[i]
-            
-#             state[i] = 1
-#             ans = True
-            
-#             for adj in graph[i]:
-#                 ans = ans and dfs(adj)
-            
-#             memo[i] = ans
-#             state[i] = 0
-            
-#             return memo[i]
-        
-#         safes = []
-#         for i in range(len(graph)):
-#             if dfs(i):
-#                 safes.append(i)
-        
-#         return safes
-
-
-        incoming = [set() for _ in range(len(graph))]
-        outgoing = [set(adj) for adj in graph]
+        parents = defaultdict(list)
+        outdegree = [0]*len(graph)
+        terminals = deque([])
         safe = [False]*len(graph)
-        terminals = []
         
-        for i,adj in enumerate(outgoing):
-            if not adj:
-                terminals.append(i)
-            for node in adj:
-                incoming[node].add(i)
+        for node,friends in enumerate(graph):
+            if friends == []:
+                safe[node] = True
+                terminals.append(node)
+                
+        for parent,children in enumerate(graph):
+            for child in children:
+                parents[child]+=[parent]
+                outdegree[parent]+=1
+        
         
         while terminals:
-            node = terminals.pop()
-            safe[node] = True
-
-            for adj in incoming[node]:
-                outgoing[adj].remove(node)
-                if len(outgoing[adj])==0:
-                    terminals.append(adj)
+            node = terminals.popleft()
+            for parent in parents[node]:
+                outdegree[parent]-=1
+                if outdegree[parent] == 0:
+                    safe[parent] = True
+                    terminals.append(parent)
         
-        return [i for i,isSafe in enumerate(safe) if isSafe]
-    
-    
+        return [node for node,isSafe in enumerate(safe) if isSafe]
+                    
+        
+        
+        
+        
+                
+        
+        
+        
+                
