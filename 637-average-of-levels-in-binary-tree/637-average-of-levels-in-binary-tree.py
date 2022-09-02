1eddfc8a423772ceb76d2c765ab95ae059ea9a31
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-02 08:06:21 +0300

    Time: 62 ms (80.44%), Space: 16.5 MB (87.46%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
@@ -6,13 +6,33 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        nodes = defaultdict(list)
-        def dfs(node,depth):
-            if node is None:
-                return
-            nodes[depth]+=[node.val]
-            dfs(node.right,depth+1)
-            dfs(node.left,depth+1)
-        depth = 0
-        dfs(root,depth)
-        return[sum(nodes[depth])/len(nodes[depth]) for depth in range(len(nodes))]
+        # nodes = defaultdict(list)
+        # def dfs(node,depth):
+        #     if node is None:
+        #         return
+        #     nodes[depth]+=[node.val]
+        #     dfs(node.right,depth+1)
+        #     dfs(node.left,depth+1)
+        # depth = 0
+        # dfs(root,depth)
+        # return[sum(nodes[depth])/len(nodes[depth]) for depth in range(len(nodes))]
+        
+        q = deque([root])
+        ans = []
+        
+        while q:
+            temp = []
+            n = 0
+            total = 0
+            
+            while q:
+                node = q.popleft()
+                total+=node.val
+                n+=1
+                if node.right: temp.append(node.right)
+                if node.left: temp.append(node.left)
+            ans.append(total/n)
+            q = deque(temp)
+        
+        return ans
+        
