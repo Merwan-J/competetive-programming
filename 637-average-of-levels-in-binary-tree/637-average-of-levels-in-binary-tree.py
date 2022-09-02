dd91cb44b699993ff25cb1ee770383f3e6fcd9d6
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-02 07:54:35 +0300

    Time: 132 ms (5.06%), Space: 17.1 MB (8.79%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
@@ -1,58 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def init(self, val=0, left=None, right=None):
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def averageOfLevels(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[float]
-        """
-        stack = collections.deque([root])
-        temp = []
-        count,total = 0,0
-        res = [root.val/1]
-        
-        
-        def friends(node):
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        nodes = defaultdict(list)
+        def dfs(node,depth):
             if node is None:
-                return []
-            ans = []
-            if node.right: ans.append(node.right)
-            if node.left: ans.append(node.left)
-            
-            return ans
-        
-        while stack:
-            v = stack.popleft()
-            
-            for node in friends(v):
-                temp.append(node)
-                count += 1
-                total += node.val
-                
-            if not stack:
-                stack = collections.deque(temp)
-                temp = []
-                if count!=0: res.append(total/count)
-                count,total = 0,0
-        return res
-        print(visited)
-
-
-
-#         nodes = dict()
-#         def dfs(node,depth):
-#             if node is None:
-#                 return 
-#             depth += 1
-#             nodes[depth] = nodes.get(depth,[])+[node.val]
-#             dfs(node.right,depth)
-#             dfs(node.left,depth)
-#         dfs(root,0)
-#         nodes = sorted(nodes.items())
-#         nodes = [sum(item[1])/len(item[1]) for item in nodes]
-        
-#         return nodes
+                return
+            nodes[depth]+=[node.val]
+            dfs(node.right,depth+1)
+            dfs(node.left,depth+1)
+        depth = 0
+        dfs(root,depth)
+        return[sum(nodes[depth])/len(nodes[depth]) for depth in range(len(nodes))]
