c83c5235cd3adede1489b7f2942cfd4e310761fd
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-03 23:43:47 +0300

    Time: 75 ms (25.14%), Space: 14.1 MB (20.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,29 +1,18 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-#         memo = defaultdict(int)
         
-#         def helper(target):
-#             if target<0:
-#                 return 0
-            
-#             if target == 0:
-#                 return 1
-#             if target in memo:
-#                 return memo[target]
-
-#             for num in nums:
-#                 memo[target] += helper(target-num)
-            
-#             return memo[target]
-                
-#         helper(target)
         
-#         return memo[target]
-        dp = [0]*(target + 1)
-        dp[0] = 1
-        for i in range(1,target+1):
+        @cache
+        def dfs(total):
+            if total == 0:
+                return 1
+            ans = 0
             for num in nums:
-                if i-num>=0:
-                    dp[i] += dp[i-num]
+                if total-num>=0:
+                    ans+= dfs(total-num)
+            return ans
         
-        return dp[target]
+        return dfs(target)
+        
+            
+            
