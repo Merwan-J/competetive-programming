c473fa7138998dfe85a6c27119bb0b483749a3ce
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-23 15:28:20 +0300

    Time: 54 ms (41.55%), Space: 14.2 MB (6.42%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,32 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-#         Top Down approach
-#         memo = {}
+        @cache
+        def dp(r,c):
+            if r==m or c==n:
+                return 0
+            if r == m-1 and c == n-1:
+                return 1
+            
+            return dp(r+1,c) + dp(r,c+1)
         
-#         def dfs(row,col):
-#             if row==m-1 and col ==n-1:
-#                 return 1
+        return dp(0,0)
             
-#             if row>=m or col>=n:
-#                 return 0
-#             if (row,col) in memo:
-#                 return memo[(row,col)]
-#             memo[(row,col)] = dfs(row+1,col) + dfs(row,col+1)
-            
-#             return memo[(row,col)]
-            
-            
-#         return dfs(0,0)
     
-    
-    
-# Bottom Up approach
-
-        grid = [[0]*(n+1) for i in range(m+1)]
-        grid[m-1][n] = 1
-        
-        for r in range(m-1,-1,-1):
-            for c in range(n-1,-1,-1):
-                grid[r][c] = grid[r+1][c] + grid[r][c+1]
-        return grid[0][0]
