e4699a68b341d7206bdf164541f634e3300da4bf
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-23 18:15:52 +0300

    Time: 79 ms (38.64%), Space: 14 MB (42.99%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -4,29 +4,29 @@ class Solution:
         n = len(grid[0])
         
         
-        @cache
-        def dp(r,c):
-            if r == m or c == n or grid[r][c] == 1:
-                return 0
+#         @cache
+#         def dp(r,c):
+#             if r == m or c == n or grid[r][c] == 1:
+#                 return 0
             
-            if r == m-1 and c == n-1:
-                return 1
+#             if r == m-1 and c == n-1:
+#                 return 1
            
-            return dp(r+1,c) + dp(r,c+1)
+#             return dp(r+1,c) + dp(r,c+1)
         
-        return dp(0,0)
+#         return dp(0,0)
     
         
-#         dp = [[0]*n for _ in range(m)]
-#         dp[m-1][n-1] = 1 if grid[m-1][n-1]!=1 else 0
+        dp = [[0]*n for _ in range(m)]
+        dp[m-1][n-1] = 1 if grid[m-1][n-1]!=1 else 0
         
-#         for r in range(m-1,-1,-1):
-#             for c in range(n-1,-1,-1):
-#                 if grid[r][c] == 1:
-#                     continue
-#                 if r+1<m:
-#                     dp[r][c] += dp[r+1][c]
-#                 if c+1<n:
-#                     dp[r][c]+=dp[r][c+1]
-#         return dp[0][0]
+        for r in range(m-1,-1,-1):
+            for c in range(n-1,-1,-1):
+                if grid[r][c] == 1:
+                    continue
+                if r+1<m:
+                    dp[r][c] += dp[r+1][c]
+                if c+1<n:
+                    dp[r][c]+=dp[r][c+1]
+        return dp[0][0]
                 
