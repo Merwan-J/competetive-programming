2b4f63ef6279f1258c2b23ddfab736190aea8d7c
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-23 15:37:24 +0300

    Time: 26 ms (98.88%), Space: 14.2 MB (10.53%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -11,5 +11,27 @@ class Solution:
             return dp(r+1,c) + dp(r,c+1)
         
         return dp(0,0)
-            
+        
+#         
+        # r x c m x n
+        
+#         basecase= dp[m-1][n-1] = 1
+#         return dp[0][0]
+        
+#         dp[r+1][c] + dp[r][c+1]
+        
+#         r = m-1 -> 0
+#         c = n-1 -> 0
+
+        dp = [[0]*n for _ in range(m)]
+        dp[m-1][n-1] = 1
+        
+        for r in range(m-1,-1,-1):
+            for c in range(n-1,-1,-1):                
+                if c+1<n:
+                    dp[r][c] += dp[r][c+1]
+                if r+1<m:
+                    dp[r][c] += dp[r+1][c]                
+        
+        return dp[0][0]
     
