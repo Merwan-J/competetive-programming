2e298fbda46c5fcbbd26c8a429ae29d6578cc793
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-24 17:51:09 +0300

    Time: 169 ms (54.30%), Space: 20 MB (8.94%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,35 +1,15 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-            # Bottom UP Approach
-#         dp = {}
+        row,col = len(grid),len(grid[0])
         
-#         def dfs(row,col):
-#             if (row==len(grid) and col==len(grid[0])-1) or(row==len(grid)-1 and col==len(grid[0])):
-#                 return 0
-#             if row>=len(grid) or col>=len(grid[0]):
-#                 return float('inf')
+        @cache
+        def dfs(r,c):
+            if r==row or c == col:
+                return inf
             
-#             if (row,col) in dp:
-#                 return dp[(row,col)]
+            if r == row - 1 and c == col -1:
+                return grid[r][c]
             
-#             dp[(row,col)] = grid[row][col] + min(dfs(row+1,col),dfs(row,col+1))
-            
-#             return dp[(row,col)]
+            return grid[r][c] + min(dfs(r+1,c),dfs(r,c+1))
         
-#         return dfs(0,0)
-
-
-
-#           Top down approach
-
-
-        row = len(grid)
-        col = len(grid[0])
-        result = [[float('inf')]*(col+1) for j in range(row+1)]
-        result[row-1][col] = 0
-        
-        for r in range(row-1,-1,-1):
-            for c in range(col-1,-1,-1):
-                result[r][c] = grid[r][c] + min(result[r+1][c],result[r][c+1])
-        
-        return result[0][0]
+        return dfs(0,0)
