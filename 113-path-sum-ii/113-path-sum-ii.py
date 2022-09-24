ee092e992eae91c0cc32fe2feab619f5217fe7ff
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-24 07:12:43 +0300

    Time: 53 ms (83.88%), Space: 15.5 MB (72.84%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -7,21 +7,40 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         
+#         ans = []
+#         def dfs(node,target,seen):
+#             if node is None:
+#                 return 
+            
+#             target-=node.val
+#             seen.append(node.val)
+            
+#             if node.right is None and node.left is None and target == 0:
+#                 ans.append(seen.copy())
+            
+#             dfs(node.right,target,seen)
+#             dfs(node.left,target,seen)
+#             seen.pop()
+            
+#         dfs(root,targetSum,[])
+#         return ans
+    
+        
         ans = []
-        def dfs(node,target,seen):
-            if node is None:
-                return 
+        q = deque([(root,targetSum,[])]) if root else []
+        
+        while q:
+            node,total,path = q.popleft()
             
-            target-=node.val
-            seen.append(node.val)
-            
-            if node.right is None and node.left is None and target == 0:
-                ans.append(seen.copy())
-            
-            dfs(node.right,target,seen)
-            dfs(node.left,target,seen)
-            seen.pop()
-            
-        dfs(root,targetSum,[])
-        return ans
+            total-=node.val
+                       
+            if node.right is None and node.left is None and total == 0:
+                ans.append(path+[node.val])
                 
+            if node.right:
+                q.append((node.right,total,path+[node.val]))
+            if node.left:
+                q.append((node.left,total,path+[node.val]))
+        
+        return ans
+            
