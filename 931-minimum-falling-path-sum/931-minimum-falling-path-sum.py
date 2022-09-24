c5607ef4eebc9337f51ed15ce2195cce79c412a4
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-24 20:01:19 +0300

    Time: 355 ms (15.35%), Space: 14.6 MB (89.76%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -1,45 +1,38 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-#         top down approach
-
-#         dp = {}
-    
-#         def dfs(row,col):
-#             if row==len(matrix)-1 and col<len(matrix[0]) and col>-1:
-#                 return matrix[row][col]
-            
-#             if row>=len(matrix) or col>=len(matrix[0]) or col<0:
-#                 return float('inf')
-            
-#             if (row,col) in dp:
-#                 return dp[(row,col)]
-            
-#             dp[(row,col)] = matrix[row][col] + min(dfs(row + 1, col - 1), dfs(row + 1, col),dfs(row + 1, col + 1))
-            
-#             return dp[(row,col)]
-        
-#         ans = float('inf')
-        
-#         for c in range(len(matrix[0])):
-#             ans = min(ans,dfs(0,c))
-#         return ans
-
-
-
-#           Bottom up approach - O(1) auxiliary space - in place
-        
         row = len(matrix)
         col = len(matrix[0])
+
+#         @cache
+#         def dfs(r,c):
+#             if r == row:
+#                 return 0
+#             if c==-1 or c==col:
+#                 return inf
+            
+#             return matrix[r][c] + min(dfs(r+1,c),dfs(r+1,c+1),dfs(r+1,c-1))
+        
+#         ans = inf
+        
+#         for i in range(col):
+#             ans = min(dfs(0,i),ans)
+        
+#         return ans
+
+        dp = matrix[-1][:]
         
         for r in range(row-2,-1,-1):
-            for c in range(col-1,-1,-1):
-                left = matrix[r+1][c-1] if c-1>-1 else float('inf')
-                right = matrix[r+1][c+1] if c+1<row else float('inf')
-                below = matrix[r+1][c]
+            prev = dp.copy()
+            for c in range(col):
                 
-                matrix[r][c] += min(left,right,below)
-        return min(matrix[0])
+                down,downleft,downright = inf,inf,inf
                 
-
-
+                if c+1<row:
+                    downright = prev[c+1]
+                if c-1>-1:
+                    downleft = prev[c-1]
+                down = prev[c]
                 
+                dp[c] = matrix[r][c] + min(down,downright,downleft)
+                
+        return min(dp)
