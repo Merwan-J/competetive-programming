fe405683d1728e3291479ca01b225a426b47907c
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-09-24 18:34:32 +0300

    Time: 81 ms (83.38%), Space: 17.4 MB (8.46%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,26 +1,13 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-#         top down
-#         dp = dict()
         
-#         def dfs(row,col):
-#             if row>=len(triangle) or col>=len(triangle[row]):
-#                 return 0
+        @cache
+        def dfs(row,col):
+            if row == len(triangle):
+                return 0
             
-#             if (row,col) in dp:
-#                 return dp[(row,col)]
-                        
-#             dp[(row,col)] = triangle[row][col] + min(dfs(row+1,col),dfs(row+1,col+1))
-#             return dp[(row,col)]
+            return triangle[row][col] + min(dfs(row+1,col),dfs(row+1,col+1))
         
-#         return dfs(0,0)
-    
-#         bottom up with O(n) space complexity
-
-        dp = [0]*(len(triangle)+1)
-        
-        for r in range(len(triangle)-1,-1,-1):
-            row = triangle[r]
-            for i,val in enumerate(row):
-                dp[i] = val + min(dp[i],dp[i+1])
-        return dp[0]
+        return dfs(0,0)
+            
+            
