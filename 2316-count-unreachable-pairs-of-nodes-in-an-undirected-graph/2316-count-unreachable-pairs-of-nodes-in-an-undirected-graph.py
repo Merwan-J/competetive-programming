b191cc4368f14465ab1ab3c06d53d702c5f1cb75
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-10-04 18:46:16 +0300

    Time: 5064 ms (24.46%), Space: 75.3 MB (68.69%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -1,29 +1,64 @@
 class Solution:
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
-        graph = [[] for _ in range(n)]
+        parent = list(range(n))
+        size = [1]*n
+        
+        def find(node):
+            if node == parent[node]:
+                return node
+            parent[node] = find(parent[node])
+            return parent[node]
+        
+        def unite(u,v):
+            a = find(u)
+            b = find(v)
+            
+            if a!=b:
+                if size[a]<size[b]:
+                    a,b = b,a
+                parent[b] = a
+                size[a]+=size[b]
+        
         for u,v in edges:
-            graph[u]+=[v]
-            graph[v]+=[u]
+            unite(u,v)
+        
+        
+        totalNodes = 0
+        ans = 0
+        for node in set([find(i) for i in range(n)]):
+            ans+=totalNodes*size[node]
+            totalNodes += size[node]
+        return ans
+        
+        
+        
             
-        def dfs(node):
-            if node in visited:
-                return 0
-            visited.add(node)
-            ans = 1
-            for adj in graph[node]:
-                if adj not in visited:
-                    ans += dfs(adj)
+        
+        
+#         graph = [[] for _ in range(n)]
+#         for u,v in edges:
+#             graph[u]+=[v]
+#             graph[v]+=[u]
             
-            return ans
+#         def dfs(node):
+#             if node in visited:
+#                 return 0
+#             visited.add(node)
+#             ans = 1
+#             for adj in graph[node]:
+#                 if adj not in visited:
+#                     ans += dfs(adj)
+            
+#             return ans
         
-        nodesSoFar = 0
-        unreachable = 0
-        visited = set()
+#         nodesSoFar = 0
+#         unreachable = 0
+#         visited = set()
         
-        for node in range(n):
-            if node not in visited:
-                curGroupMembers = dfs(node)
-                unreachable += nodesSoFar*curGroupMembers
-                nodesSoFar += curGroupMembers
+#         for node in range(n):
+#             if node not in visited:
+#                 curGroupMembers = dfs(node)
+#                 unreachable += nodesSoFar*curGroupMembers
+#                 nodesSoFar += curGroupMembers
         
-        return unreachable
+#         return unreachable
