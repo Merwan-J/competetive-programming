e089439489811ba983ce6d738d1d3305d0ab7f9c
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-10-06 07:43:33 +0300

    Time: 185 ms (29.82%), Space: 14.5 MB (37.95%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        dpMax = [nums[0]]*len(nums)
-        dpMin = [nums[0]]*len(nums)
-        
-        for i in range(1,len(nums)):
-            cur = nums[i]
-            dpMin[i] = min(dpMax[i-1]*cur,dpMin[i-1]*cur,cur)
-            dpMax[i] = max(dpMax[i-1]*cur,dpMin[i-1]*cur,cur)
-            
-            # if cur>0:
-            #     dpMax[i] = max(dpMax[i-1]*cur,cur)
-            #     dpMin[i] = min(dpMin[i-1]*cur,cur)
-            # else:
-            #     dpMax[i] = max(dpMin[i-1]*cur,cur)
-            #     dpMin[i] = min(dpMax[i-1]*cur,cur)
-        
-        return max(dpMax)
+        the_max, the_min = nums[0] , nums[0]
+        ans = the_max
+
+        for idx in range(1, len(nums)):
+            new_max = max(the_min * nums[idx], the_max * nums[idx], nums[idx])
+            new_min = min(the_max * nums[idx], the_min * nums[idx], nums[idx])
+
+            the_max, the_min = new_max, new_min
+            ans = max(ans, the_max)
+
+        return ans
+    
+    
