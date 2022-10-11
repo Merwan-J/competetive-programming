083474a9c04cb8126cab21b2495fa2be29394292
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-10-11 14:16:01 +0300

    Time: 173 ms (21.64%), Space: 28.4 MB (6.26%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,24 +8,50 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        self.anc = None
-        def dfs(node):
-            if node is None:
-                return False
+#         self.anc = None
+#         def dfs(node):
+#             if node is None:
+#                 return False
+                        
+#             left = dfs(node.left)
+#             right = dfs(node.right)
+#             cur = node == q or node == p
             
-            left,cur,right = False,False,False
+#             if cur + right + left == 2:
+#                 self.anc = node
             
-            left = dfs(node.left)
-            right = dfs(node.right)
-            cur = node == q or node == p
-            
-            if cur + right + left == 2:
-                self.anc = node
-            
-            return left or right or cur
+#             return left or right or cur
         
-        dfs(root)
-        return self.anc
+#         dfs(root)
+#         return self.anc
+
+        parent = {}
+    
+        def dfs(node,p):
+            if node is None:
+                return 
+            parent[node] = p
+            
+            dfs(node.right,node)
+            dfs(node.left,node)
+            
+        
+        dfs(root,None)
+        
+        ancestors = set()
+        ancestors.add(q)
+        
+        while q:
+            ancestors.add(parent[q])
+            q = parent[q]
+        
+        while p not in ancestors:
+            p = parent[p]
+        
+        return p
+        
+        
+            
             
             
                 
