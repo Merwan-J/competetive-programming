84c57b0362149778e00298c5de4bb31aae1c046e
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-10-11 22:03:55 +0300

    Time: 1229 ms (5.01%), Space: 15.2 MB (7.67%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,39 +1,63 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+#         n = len(edges)
+#         parent = [i for i in range(n+1)]
+#         size = [1]*(n+1)
+        
+#         def find(node):
+#             if parent[node] == node:
+#                 return node
+#             parent[node] = find(parent[node])
+#             return parent[node]
+        
+#         def union(u,v):
+#             a = find(u)
+#             b = find(v)
+            
+#             if size[a]<size[b]:
+#                 a,b = b,a
+            
+#             size[a]+=size[b]
+#             parent[b] = a
+            
+        
+#         ans = set()
+        
+#         for u,v in edges:
+#             if find(u) == find(v):
+#                 ans.add((u,v))
+#             else:
+#                 union(u,v)
+        
+#         for u,v in reversed(edges):
+#             if (u,v) in ans:
+#                 return [u,v]
         n = len(edges)
-        parent = [i for i in range(n+1)]
-        size = [1]*(n+1)
-        
-        def find(node):
-            if parent[node] == node:
-                return node
-            parent[node] = find(parent[node])
-            return parent[node]
-        
-        def union(u,v):
-            a = find(u)
-            b = find(v)
-            
-            if size[a]<size[b]:
-                a,b = b,a
-            
-            size[a]+=size[b]
-            parent[b] = a
-            
-        
-        ans = set()
+        graph = [[] for _ in range(n)]
         
         for u,v in edges:
-            if find(u) == find(v):
-                ans.add((u,v))
-            else:
-                union(u,v)
+            graph[u-1].append(v-1)
+            graph[v-1].append(u-1)
         
+        
+        def dfs(node,prev):
+            visited.add(node)
+            for adj in graph[node]:
+                if adj!=prev:
+                    if adj in visited:
+                        ans.add((node+1,adj+1))
+                    else:
+                        dfs(adj,node)
+        ans = set()
+        
+        for node in range(n):
+            visited = set()
+            dfs(node,-1)
+            
         for u,v in reversed(edges):
             if (u,v) in ans:
                 return [u,v]
         
-
         
         
             
