ae98d10646cd29d60d0a846da8ab6008dea3d0f5
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-10-13 10:31:25 +0300

    Time: 358 ms (11.63%), Space: 16.9 MB (51.43%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -7,17 +7,37 @@
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         
-        def dfs(node,val):
-            if val>node.val:
-                if node.right:
-                    return dfs(node.right,val)
-                node.right = TreeNode(val)
-            else:
-                if node.left:
-                    return dfs(node.left,val)
-                node.left = TreeNode(val)
+#         def dfs(node,val):
+#             if val>node.val:
+#                 if node.right:
+#                     return dfs(node.right,val)
+#                 node.right = TreeNode(val)
+#             else:
+#                 if node.left:
+#                     return dfs(node.left,val)
+#                 node.left = TreeNode(val)
         
+#         if root is None:
+#             return TreeNode(val)
+#         dfs(root,val)
+#         return root
+
         if root is None:
             return TreeNode(val)
-        dfs(root,val)
-        return root
+        
+        node = root
+        
+        while node:
+            if val>node.val:
+                if node.right:
+                    node = node.right
+                else:
+                    node.right = TreeNode(val)
+                    return root
+            else:
+                if node.left:
+                    node = node.left
+                else:
+                    node.left = TreeNode(val)
+                    return root
+        
