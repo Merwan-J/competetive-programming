f7ef8e60cbbd549538ce813e34fb7fbb603775c3
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-10-22 01:45:35 +0300

    Time: 2729 ms (45.40%), Space: 29.5 MB (39.90%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,17 +1,35 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total = sum(nums)
-        self.found = False
+#         total = sum(nums)
+#         self.found = False
         
-        @cache
-        def dp(i,cur):
-            nonlocal total
-            if total-cur == cur or self.found:
-                self.found = True
-                return True
-            if i == len(nums) or total-cur<cur:
-                return False
+#         @lru_cache(None)
+#         def dp(i,cur):
+#             nonlocal total
+#             if total-cur == cur or self.found:
+#                 self.found = True
+#                 return True
+#             if i == len(nums) or total-cur<cur:
+#                 return False
             
-            return dp(i+1,cur+nums[i]) or dp(i+1,cur)
+#             return dp(i+1,cur+nums[i]) or dp(i+1,cur)
         
-        return dp(0,0)
+#         return dp(0,0)
+        target = sum(nums)//2
+        if target!=sum(nums)-target:
+            return False
+        
+        n = len(nums)
+        dp = [[False]*(target+1) for r in range(n)]
+        dp[0][0] = True
+        
+        for i in range(n-1):
+            for j in range(target+1):
+                if dp[i][j]:
+                    dp[i+1][j] = True
+                    if j+nums[i]<=target:
+                        dp[i+1][j+nums[i]] = True
+        
+        return sum([dp[i][target] for i in range(n) if dp[i][target] == True]) > 0
+                    
+                    
