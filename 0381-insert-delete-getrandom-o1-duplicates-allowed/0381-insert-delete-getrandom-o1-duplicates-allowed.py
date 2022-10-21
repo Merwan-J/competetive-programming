d612f0967e0179fb2de9ad8d2ac19df952b80fc9
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-10-21 05:26:07 +0300

    Time: 1521 ms (6.34%), Space: 65.9 MB (52.33%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.py
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.py
@@ -5,17 +5,15 @@ class RandomizedCollection:
         self.hmap = defaultdict(set)
 
     def insert(self, val: int) -> bool:
-        # print(self.arr,"before inserting")
         self.arr.append(val)
         self.hmap[val].add(len(self.arr)-1)
-        # print(self.arr,"after inserting",len(self.hmap[val]) == 1)
+        
         return len(self.hmap[val]) == 1
 
     def remove(self, val: int) -> bool:
-        # print(self.hmap[val],len(self.hmap[val]),"checking")
         if len(self.hmap[val]) == 0:
             return False
-        # print(self.arr,"before removing",val,self.hmap)
+        
         index = self.hmap[val].pop()        
         lastNum = self.arr[-1]
         n = len(self.arr)
@@ -26,14 +24,14 @@ class RandomizedCollection:
         self.hmap[lastNum].discard(n-1)
         if index != n-1:
             self.hmap[lastNum].add(index)
-        # print(self.arr,"after removing",self.hmap)
+            
         return True
     
     def getRandom(self) -> int:
         start = 0
         end = len(self.arr)-1
-        # print(start,end,self.arr)
         index = random.randint(start,end)
+        
         return self.arr[index]
 
 
