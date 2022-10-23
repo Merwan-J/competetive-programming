ebf3725b6e19cc44482c4f40810fb806902cdb47
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-10-23 21:32:56 +0300

    Time: 1483 ms (6.69%), Space: 14.2 MB (80.42%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,18 +1,36 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
-        @cache
-        def dp(i,total):
-            if i == 0:
-                ans = 0
-                if total+nums[i] == target: ans+=1
-                if total-nums[i] == target: ans+=1
-                return ans
+#         @cache
+#         def dp(i,total):
+#             if i == 0:
+#                 ans = 0
+#                 if total+nums[i] == target: ans+=1
+#                 if total-nums[i] == target: ans+=1
+#                 return ans
             
-            add = dp(i-1,total+nums[i])
-            subtract = dp(i-1,total-nums[i])
+#             add = dp(i-1,total+nums[i])
+#             subtract = dp(i-1,total-nums[i])
             
-            return add + subtract
+#             return add + subtract
         
         
-        return dp(len(nums)-1,0)
+#         return dp(len(nums)-1,0)
+    
+        dp = [[0]*2001 for _ in range(len(nums))]
+        
+        dp[0][nums[0]] = 1
+        dp[0][-nums[0]%2001] += 1
+        
+        for i in range(1,len(nums)):
+            for cur in range(-1000,1001):
+                add = dp[i-1][(cur + nums[i])%2001]
+                subtract = dp[i-1][(cur-nums[i])%2001]
+                
+                dp[i][cur%2001] = add + subtract
+        
+        return dp[len(nums)-1][target]
+        
+    
+        
+        
