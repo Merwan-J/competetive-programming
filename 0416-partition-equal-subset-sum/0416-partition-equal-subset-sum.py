758c4a5be4e9f668c2410c854259986e7e162622
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-10-23 07:57:28 +0300

    Time: 7543 ms (8.52%), Space: 29.6 MB (38.59%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,35 +1,26 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-#         total = sum(nums)
-#         self.found = False
+        total = sum(nums)
+        n = len(nums)
         
-#         @lru_cache(None)
-#         def dp(i,cur):
-#             nonlocal total
-#             if total-cur == cur or self.found:
-#                 self.found = True
-#                 return True
-#             if i == len(nums) or total-cur<cur:
-#                 return False
-            
-#             return dp(i+1,cur+nums[i]) or dp(i+1,cur)
-        
-#         return dp(0,0)
-        target = sum(nums)//2
-        if target!=sum(nums)-target:
+        if total - total//2 != total//2:
             return False
         
-        n = len(nums)
-        dp = [[False]*(target+1) for r in range(n)]
-        dp[0][0] = True
+        total = total//2
+        dp = [[False]*(total+1) for row in range(n)]
         
-        for i in range(n-1):
-            for j in range(target+1):
-                if dp[i][j]:
-                    dp[i+1][j] = True
-                    if j+nums[i]<=target:
-                        dp[i+1][j+nums[i]] = True
+        for r in range(n):
+            dp[r][0] = True
         
-        return sum([dp[i][target] for i in range(n) if dp[i][target] == True]) > 0
-                    
-                    
+        if nums[0]<=total:
+            dp[0][nums[0]] = True
+        
+        for i in range(1,n):
+            for target in range(total+1):
+                not_pick = dp[i-1][target]
+                pick = False
+                if target>=nums[i]:
+                    pick = dp[i-1][target-nums[i]]
+                dp[i][target] = pick or not_pick
+        
+        return dp[n-1][total]
