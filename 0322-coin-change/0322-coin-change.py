8b08ae17d0d0782c981eb364946a115d8d176f92
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-10-23 19:24:08 +0300

    Time: 4058 ms (23.74%), Space: 14.2 MB (85.73%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,21 +2,30 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
         
-        @cache
-        def dp(target):
-            if target == 0:
-                return 0
-            if target<0:
-                return inf
+#         @cache
+#         def dp(target):
+#             if target == 0:
+#                 return 0
             
-            ans = inf
+#             ans = inf
             
+#             for coin in coins:
+#                 if target<0:
+#                     ans = min(ans,1 + dp(target-coin))
+            
+#             return ans
+        
+#         ans = dp(amount)
+        
+#         return -1 if ans == inf else ans
+        
+        dp = [inf]*(amount+1)
+        dp[0] = 0
+        
+        for cur in range(1,amount+1):
             for coin in coins:
-                ans = min(ans,1 + dp(target-coin))
-            
-            return ans
+                if cur-coin>=0:
+                    dp[cur] = min(dp[cur],dp[cur-coin] + 1)
         
-        ans = dp(amount)
+        return -1 if dp[amount] == inf else dp[amount]
         
-        return -1 if ans == inf else ans
-    
