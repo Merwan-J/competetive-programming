b4ba2328d395216b6717cec1fe4eae4e6d1fec9b
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-11-08 10:02:14 +0300

    Time: 79 ms (10.88%), Space: 13.8 MB (72.02%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.py
@@ -0,0 +1,23 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        ans = set()
+        
+        def dfs(i,j,count,h,m):
+            if count == 0:
+                minute = str(m)
+                if len(minute) == 1:
+                    minute = "0"+minute
+                ans.add(str(h)+":"+minute)
+                return
+            
+            if i >= 4 and j>=6:
+                return
+            dfs(i+1,j+1,count,h,m)
+            if i<4 and h+2**i < 12:
+                dfs(i+1,j,count-1,h+2**i,m)
+                
+            if j<6 and m+2**j < 60:
+                dfs(i,j+1,count-1,h,m+2**j)
+        
+        dfs(0,0,turnedOn,0,0)
+        return list(ans)
