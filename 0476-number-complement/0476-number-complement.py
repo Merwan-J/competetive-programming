bc64df0eeb6d267e0c1be85373135cf775c0cfa4
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2022-11-08 09:17:03 +0300

    Time: 41 ms (80.14%), Space: 13.8 MB (95.56%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,9 +1,13 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        n = floor(log(num,2))
+#         n = floor(log(num,2))
         
-        for i in range(n+1):
-            mask = 1<<i
-            num^=mask
+#         for i in range(n+1):
+#             mask = 1<<i
+#             num^=mask
         
-        return num
+#         return num
+
+        mask = 1<<(len(bin(num))-2)
+        
+        return (mask-1)^num
