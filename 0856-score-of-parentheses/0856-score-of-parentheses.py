f56f2bc3a5e815d336a166a530ce29e5ca2a61a1
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2022-11-14 11:42:23 +0300

    Time: 53 ms (58.82%), Space: 13.8 MB (62.71%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -0,0 +1,34 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        
+    
+        # initalize a stack
+        stack = []
+        
+        # traverse the string
+        for char in s:
+        # if encountered a ( : push it to the stack
+            if char == "(":
+                stack.append(char)
+                             
+        # if encountered ) and if the top of the stack is (
+            # -pop it and replace it with 1 i.e the score of it
+            elif char == ")" and stack[-1] == "(":
+                stack.pop()
+                stack.append(1)
+                             
+        # if enountered ) and the top of the stack is a number, 
+            # - pop from the top of the stack until no number is at the top of the stack, then if              the stack is empty append the sum of those number else pop the elt at the top("("              opening bracket) and push the 2 * sum  to the stack
+            elif char == ")" and stack[-1]!="(":
+                cur = 0
+                while stack and stack[-1]!="(":
+                    cur+=stack.pop()
+                if stack:
+                    stack.pop()
+                    cur = 2 * cur
+                stack.append(cur)
+                
+        return sum(stack)
+    
+    
+    
