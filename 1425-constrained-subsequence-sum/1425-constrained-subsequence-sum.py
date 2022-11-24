c5f09776ba98cf0725f705b8bcc6de9a3a8b6ce1
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2022-11-24 18:09:02 +0300

    Time: 5972 ms (10.22%), Space: 37.4 MB (5.11%) - LeetHub

---
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
@@ -0,0 +1,67 @@
+class Solution:
+    def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
+#         what is the question asking?
+#         -build a a subsequence that results a maximum sum but each consequ. elts are at most k pos apart
+        
+#         typical dp on subsequence
+        
+#         i can ask myself what is the maximum sum I can build from this position
+        
+#         i have two chioces to make: either to start a new subsequence or continue what I have previously built 
+            
+        
+            
+#         start a new subsequence: just include the current num, nums[i]
+#         continue: i can choose any previous max sum then return max(nums[i]+prevmax,prevmax)
+        
+#         then dp[i] = max(startnewsub, continue)
+#             dp[i] = max(nums[i],nums[i]+prevMax,prevMax)
+        
+#         I think in the relation putting prevMax results a probelm 
+#         prove: here u go
+#         [10,2,-10,-5,-20]
+        
+#         [10,10,10,10,10] max sum for each window
+            
+#         [10,-2,-10,-5,20], k = 2
+        
+#         so I need to change the way I am looking at the probelm because the realtion doesn't           work specially the prevMax thing
+        
+#         so if instead think of dp[i] as the maxsum of a valid subsequence that is ending at           this given position including the current number, nums[i]. this way we can prevent             cases where prevMax wont include sum out of our k sized window
+        
+#         [10]
+#         [0]
+        
+#         [10,8]
+#         [10,10]
+        
+#         [10,8,-2]
+#         [10,8,8]
+#         [10,10,0]
+        
+#         [10,8,-2,-7]
+#         [10,8,8,3]
+#         [10,10,0,-5]
+        
+        
+#         i will need a heap to get me the valid max sum in logn time 
+#         I will pop from the heap if the max is not in the k sized window 
+#         1D dp array to store the max sum in each position 
+        
+        n = len(nums)
+        heap = [(-nums[0],0)]
+        dp = [0]*n
+        dp[0] = nums[0]
+        
+        
+        for i in range(1,n):
+            while heap[0][1]<(i-k):
+                heappop(heap)
+            dp[i] = nums[i] + max(0,-heap[0][0])
+            heappush(heap,(-dp[i],i))
+        
+        return max(dp)
+        
+        
+        
+        
