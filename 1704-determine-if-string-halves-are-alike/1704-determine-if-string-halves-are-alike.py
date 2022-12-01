8d3779d12077035b3632266b60dac6ccdd54de25
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2022-12-01 09:36:49 +0300

    Time: 79 ms (25.52%), Space: 13.9 MB (33.58%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -1,16 +1,17 @@
 class Solution:
-    def halvesAreAlike(self, s: str) -> bool:
-        first = 0
-        second = 0
-        vowels = set(['a','e','i','o','u'])
-        s = s.lower()
+    def halvesAreAlike(self, s: str) -> bool:        
+        l = 0
+        r = len(s)- 1
         
-        for i in range(len(s)//2):
-            if s[i] in vowels:
-                first += 1
+        left_count = 0
+        right_count = 0
         
-        for i in range(len(s)//2,len(s)):
-            if s[i] in vowels:
-                second += 1
-        
-        return first == second
+        while l<r:
+            if s[l] in  ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'):
+                left_count+=1
+            if s[r] in  ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'):
+                right_count+=1
+            
+            l+=1
+            r-=1
+        return left_count == right_count
