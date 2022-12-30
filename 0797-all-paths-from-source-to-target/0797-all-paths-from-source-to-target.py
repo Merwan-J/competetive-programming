ebc7bc9464ba1ed6299429e9c720248f6afd70ea
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2022-12-30 20:11:18 +0300

    Time: 102 ms (88.68%), Space: 15.8 MB (22.19%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -0,0 +1,22 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        n = len(graph)
+        ans = []
+        
+        q = deque([(0,[0])])
+        
+        
+        while q:
+            node,path = q.popleft()
+            
+            if node == n-1:
+                ans.append(path)
+                continue
+            
+            for adj in graph[node]:
+                q.append((adj,path+[adj]))
+        
+        return ans
+        
+        
+                
