253729ff9fe1e4699364e06f1b33c22326c04201
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2023-01-03 08:21:14 +0300

    Time: 4235 ms (5.01%), Space: 61.7 MB (90.10%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,32 +1,44 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        # Sort based on min task processing time or min task index.
-        next_task: List[Tuple[int, int]] = []
-        tasks_processing_order: List[int] = []
+    
+#     two things 
+#         put tasks into queue when they are available
         
-        # Store task enqueue time, processing time, index.
-        sorted_tasks = [(enqueue, process, idx) for idx, (enqueue, process) in enumerate(tasks)]
-        sorted_tasks.sort()
-        
-        curr_time = 0
-        task_index = 0
-        
-        # Stop when no tasks are left in array and heap.
-        while task_index < len(tasks) or next_task:
-            if not next_task and curr_time < sorted_tasks[task_index][0]:
-                # When the heap is empty, try updating curr_time to next task's enqueue time. 
-                curr_time = sorted_tasks[task_index][0]
-            
-            # Push all the tasks whose enqueueTime <= currtTime into the heap.
-            while task_index < len(sorted_tasks) and curr_time >= sorted_tasks[task_index][0]:
-                _, process_time, original_index = sorted_tasks[task_index]
-                heapq.heappush(next_task, (process_time, original_index))
-                task_index += 1
-            
-            process_time, index = heapq.heappop(next_task)
-            
-            # Complete this task and increment curr_time.
-            curr_time += process_time
-            tasks_processing_order.append(index)
-        
-        return tasks_processing_order
+#         process available tasks
+    
+#     track current time
+    
+    
+#     sort tasks
+#     put tasks with the same enque Time as the current Time into queue params:processTime and index
+    
+    
+    
+#     when to process?
+#     how and when to increment the curTime?
+#     what happens when queue is empty and the current time is less than the smalles enque time?
+#     what happens in the end?
+
+
+        tasks = [tasks[i]+[i] for i in range(len(tasks))]
+        tasks.sort()
+        heap = []
+        ans = []
+        curTime = 0
+
+        for task in tasks:
+            enqueTime,processTime,i = task
+            while heap and curTime<enqueTime:
+                currentProcessTime,idx = heappop(heap)
+                ans.append(idx)
+                curTime+=currentProcessTime
+
+            if curTime<enqueTime:
+                curTime = enqueTime
+            heappush(heap,(processTime,i))
+
+        while heap:
+            currentProcessTime,idx = heappop(heap)
+            ans.append(idx)
+
+        return ans
