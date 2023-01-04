3b64cb87b16cb134b7d812272552a39368110314
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2023-01-04 07:12:28 +0300

    Time: 1246 ms (57.54%), Space: 28.4 MB (60.98%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -6,13 +6,11 @@ class Solution:
         for count in tasks:
             if count <2:
                 return -1
-            
-            if count%3 == 0 and count%2==0:
-                rounds+=count//3
-            elif count%3==0:
+
+            if count%3 == 0:
                 rounds+=count//3
             else:
-                rounds+=min(count//2,count//3 + 1)
+                rounds+=count//3 + 1
         
         return rounds
             
