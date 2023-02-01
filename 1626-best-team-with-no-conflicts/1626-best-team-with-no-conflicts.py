807c36be8559947dafe32f5ea495e7fa962ff7cc
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2023-02-01 20:44:19 +0300

    Time: 2066 ms (60.14%), Space: 14.1 MB (83.92%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,6 +1,5 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-#         change the question to longest increasing subsequence type after sorting the tuples by age and just look for the path that yields the maximum sum
         n = len(ages)
         arr = sorted((ages[i],scores[i]) for i in range(n))
         dp = [score for age,score in arr]
