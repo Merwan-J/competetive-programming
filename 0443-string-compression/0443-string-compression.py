de271da031afff7fbb776fdd45b1a4135ce3efb3
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2023-03-02 21:40:28 +0300

    Time: 68 ms (28.47%), Space: 13.9 MB (65.01%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -4,7 +4,7 @@ class Solution:
         prevChar = chars[0]
         count = 1
         insertPos = 1 
-        lastseen = 1
+        
         for i in range(1,n):
             curChar = chars[i]
             
@@ -12,13 +12,11 @@ class Solution:
                 prevChar = curChar
                 insertPos += len(str(count)) + 1 if count>1 else 1
                 chars[insertPos-1] = curChar
-                lastseen = insertPos - 1
                 count = 1 
                 continue 
             
             count+=1            
             for j in range(len(str(count))):
                 chars[insertPos+j] = str(count)[j] 
-                lastseen = insertPos + j
-        
-        return lastseen+1
+                
+        return insertPos + len(str(count)) if count > 1 else insertPos
