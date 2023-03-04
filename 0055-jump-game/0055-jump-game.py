f105ae214134398101dfdd7df66b0cd6a8df962c
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2023-03-04 07:00:54 +0300

    Time: 502 ms (59.40%), Space: 15.2 MB (43.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,81 +1,13 @@
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        """
-        
-        what is the question asking?
-            - check if I can reach the end of the array or not( last index )
-        what are the values of the array represent?
-            - the maximum jumps I can take from that given index
-            - I can make a jump any index less or equal to the maximum jump jump
-        
-        line sweep??
-        
-        [2,3,1,1,4]
-        [0,0,0,0,0]
-        
-        [1,0,0,-1,0]
-        [1,1,0,-1,0]
-        [1,1,1,-1,-1]
-        [2,3,1,0,-1]
-        
-        [2,5,6,6,5]
-        
-        [3,2,1,0,4]
-        [0,0,0,0,0]
-        
-        [1,0,0,0,-1]
-        [1,1,0,0,-2]
-        [1,1,1,0,-3]
-        
-        [1,2,3,3,0]
-        
-        
-        [1,0,1,1,4]
-        [0,0,0,0,0]
-        
-        [0,1,-1,0,0]
-        [0,1,-1,0,0]
-        [0,1,-1,1,-1]
-        [0,1,-1,1,0]
-        [0,1,0,1,1]
-        
-        [1,0,-1,0,0]
-        [1,0,-1,0,0]
-        [1,0,]
-        
-        """ 
-#         n = len(nums)
-#         sweep = [0]*n
-#         sweep[0] = 1
-        
-#         for i in range(n-1):
-#             jump = nums[i]
-#             start = i+1
-#             end = start+jump
-            
-#             sweep[start] += 1
-#             if end<n:
-#                 sweep[end] -= 1
-        
-#         psum = [0]
-#         for i in range(1,n):
-#             psum.append(psum[-1]+sweep[i])
-        
-#         return all(psum[1:])
+    def canJump(self, nums: List[int]) -> bool:     
+        curJump = 0
 
-
-        n = len(nums)
-        prevJump = 1
-        
-        for i in range(n-1):
-            curJump = nums[i]
+        for i,jump in enumerate(nums):
+            curJump-=1 
+            curJump = max(curJump,jump)
             
-            prevJump-=1
-            prevJump = max(prevJump,curJump)
+            if curJump == 0: break
+        
+        
+        return i == len(nums)-1
             
-            if prevJump == 0:
-                return False
-        
-        return True
-        
-        
