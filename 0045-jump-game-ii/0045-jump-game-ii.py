a9e54c014c87862158a3ae74247799cd59542f12
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2023-03-05 09:09:35 +0300

    Time: 3981 ms (25.32%), Space: 15.6 MB (11.07%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -20,15 +20,37 @@ class Solution:
 #         return dp(0)
         
         
-        n = len(nums)
-        dp = [inf] * n
-        dp[n-1] = 0
+#         n = len(nums)
+#         dp = [inf] * n
+#         dp[n-1] = 0
         
-        for i in range(n-2,-1,-1):
-            for j in range(1,nums[i]+1):
-                if i+j>=n: break
-                dp[i] = min(dp[i],dp[i+j] + 1)
+#         for i in range(n-2,-1,-1):
+#             for j in range(1,nums[i]+1):
+#                 if i+j>=n: break
+#                 dp[i] = min(dp[i],dp[i+j] + 1)
         
-        return dp[0]
+#         return dp[0]
+
+
+        q = deque([0])
+        levels = 0
+        visited = set()
+        
+        while q:
+            n = len(q)
+            for _ in range(n):
+                curIndex = q.popleft()
+                if curIndex == len(nums)-1:
+                    return levels 
+                
+                for jump in range(1,nums[curIndex]+1):
+                    nextDest = curIndex+jump
+                    if nextDest >= len(nums) or nextDest in visited:
+                        continue
+                    q.append(nextDest)
+                    visited.add(nextDest)      
+            levels+=1
+        
+        return levels 
         
                 
