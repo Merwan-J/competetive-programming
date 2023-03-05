0f77fdc06d3864ecfd2ff57276fc285eae51013a
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2023-03-05 08:30:25 +0300

    Time: 7937 ms (14.27%), Space: 15 MB (51.04%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -4,20 +4,31 @@ class Solution:
         
         
         
-        @cache
-        def dp(i):
-            if i >= len(nums)-1:
-                return 0
+#         @cache
+#         def dp(i):
+#             if i >= len(nums)-1:
+#                 return 0
             
-            ans = inf
+#             ans = inf
             
-            for j in range(1,nums[i]+1):
-                ans = min(ans,dp(i+j))
+#             for j in range(1,nums[i]+1):
+#                 ans = min(ans,dp(i+j))
             
-            return ans + 1
+#             return ans + 1
                 
         
-        return dp(0)
+#         return dp(0)
         
-            
+        
+        n = len(nums)
+        dp = [inf] * n
+        dp[n-1] = 0
+        
+        for i in range(n-2,-1,-1):
+            for j in range(1,nums[i]+1):
+                if i+j>=n: break
+                dp[i] = min(dp[i],dp[i+j] + 1)
+        
+        return dp[0]
+        
                 
