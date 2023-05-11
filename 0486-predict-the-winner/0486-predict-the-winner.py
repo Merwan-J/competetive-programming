5e3c31e18caf6d55e2ca94376092a48f7bf71a87
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2023-05-11 23:40:36 +0300

    Time: 9924 ms (5.05%), Space: 16.2 MB (19.24%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,18 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        
+        def findTheWinner(l,r,turn):
+            if l>r:
+                return 0
+            
+            sign = 1 if turn else -1
+            
+            left = sign * nums[l]  + findTheWinner(l+1,r,not turn)
+            right = sign * nums[r] + findTheWinner(l,r-1,not turn)
+            
+            if turn:
+                return max(left,right)
+            return min(left,right)
+        
+        
+        return findTheWinner(0,len(nums)-1,True)>=0
