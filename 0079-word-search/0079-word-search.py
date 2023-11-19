5b4876c4ee81a29c89d300ca5f34322527b0695b
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2023-11-19 14:05:09 +0300

    Time: 6644 ms (28.26%), Space: 16.4 MB (40.52%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,34 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        row,col = len(board),len(board[0])
-        self.found = False
+        
         
         def dfs(r,c,i):
-            if i == len(word):
-                self.found = True
-                return True
+            if i == len(word)-1 and board[r][c]==word[i]:
+                return True 
             
-            if self.found:
-                return True
+            path.add((r,c))
+            ans = False 
             
-            temp = board[r][c]
-            board[r][c] = "#"
-            
-            ans = False
-            for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)]:
-                nr,nc = r+dr,c+dc
-                if -1<nr<row and -1<nc<col and board[nr][nc] == word[i]:   
-                    ans = ans or dfs(nr,nc,i+1)
-            board[r][c] = temp
-            return ans
+            for x,y in [(1,0),(0,1),(-1,0),(0,-1)]:
+                nr,nc = r+x,c+y
+                
+                # inbound, match go 
+                if (-1<nr<len(board) and -1<nc<len(board[0]) and (nr,nc) not in path and
+                    i+1<len(word) and board[nr][nc]==word[i+1]):
+                    ans = ans or dfs(nr,nc,i+1)                
+            path.remove((r,c))
+            return ans 
         
-        
-        for r in range(row):
-            for c in range(col):
-                if board[r][c] == word[0] and dfs(r,c,1):
-                    return True
+        # visited clear it every time you get starting letter 
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == word[0]:
+                    path = set() 
+                    if dfs(r,c,0): return True
         
         return False
-                    
-                
-                
